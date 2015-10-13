d419405c4bbf33e8824eea842d5b0983a635eed9
Eric Promislow <eric.promislow@hpe.com>
2015-10-13 10:35:03 -0700

    Remove hard-wired referencs to a personal key file

--- terraform-scripts/run-terraform.sh
+++ terraform-scripts/run-terraform.sh
@@ -1,13 +1,31 @@
 #!/bin/sh 
 
-set -ex
+set -e
 
 case $# in
   0) args=apply ;;
   *) args="$@" ;;
 esac
 
-TF_VAR_key_file=${KEY_FILE:-ericp01.pem} \
+problems=0
+for var in KEY_FILE OS_USERNAME OS_PASSWORD RUNTIME_USERNAME ; do
+  eval val=$(echo \$$var)
+  if [ -z "$val" ] ; then
+    echo "$var is unset"
+    problems=1
+  fi
+done
+case $problems in
+  1) exit 1 ;;
+esac
+
+if [ ! -f "${KEY_FILE}" ] ; then
+  echo "File ${KEY_FILE} not found"
+  exit 2
+fi
+
+TF_VAR_key_pair=$(basename $KEY_FILE .pem) \
+    TF_VAR_key_file=$KEY_FILE \
     TF_VAR_os_user=$OS_USERNAME \
     TF_VAR_os_password=$OS_PASSWORD \
     TF_VAR_runtime_username=$RUNTIME_USERNAME \
