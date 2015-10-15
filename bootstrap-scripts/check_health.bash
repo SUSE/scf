5a86eaa5259b2ff88c75c46ae451af0041e617f4
Aaron Lefkowitz <aaron.lefkowitz@hpe.com>
2015-10-15 15:44:22 -0700

    Make health checks more robust
    - Make the output on a monit-failing health check more useful instead of
      spewing XML errors.
    - Fix the consul base image (needed apt-get update to find cool packages)

--- bootstrap-scripts/check_health.bash
+++ bootstrap-scripts/check_health.bash
@@ -9,7 +9,12 @@ job_names="$@"
 is_unhealthy=0
 
 for service_name in ${job_names}; do
-  service_health=$(curl -s -u "${monit_user}:${monit_pass}" "http://127.0.0.1:${monit_port}/_status?format=xml" | xmlstarlet sel -t -m "monit/service[name='${service_name}']" -v status)
+  monit_status=$(curl -s -u "${monit_user}:${monit_pass}" "http://127.0.0.1:${monit_port}/_status?format=xml")
+  if [[ "$?" != 0 ]]; then
+    echo "failed to reach monit at: http://127.0.0.1:${monit_port} using provided username and password"
+    exit 2
+  fi
+  service_health=$(echo "${monit_status}" | xmlstarlet sel -t -m "monit/service[name='${service_name}']" -v status)
   echo "${service_name} => ${service_health}"
   if [[ 0 != "${service_health}" ]]; then
     is_unhealthy=2
