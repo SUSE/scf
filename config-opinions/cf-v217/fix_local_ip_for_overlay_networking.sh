0522e334ff7b42b10f543b6d3091d056b80ac2e4
Vlad Iovanov <vlad.iovanov@hp.com>
2015-10-21 13:06:36 -0700

    Make overlay gateway configurable via env var

--- config-opinions/cf-v217/fix_local_ip_for_overlay_networking.sh
+++ config-opinions/cf-v217/fix_local_ip_for_overlay_networking.sh
@@ -4,7 +4,12 @@ if [[ "${HCF_NETWORK}" -ne "overlay" ]]; then
   exit 0
 fi
 
-route add 198.41.0.4 gw 192.168.252.1 dev eth0
-route add 1.2.3.4 gw 192.168.252.1 dev eth0
+if [[ -z "${HCF_OVERLAY_GATEWAY}" ]]; then
+  echo "Missing HCF_OVERLAY_GATEWAY environment variable."
+  exit 1
+fi
+
+route add 198.41.0.4 gw $HCF_OVERLAY_GATEWAY dev eth0
+route add 1.2.3.4 gw $HCF_OVERLAY_GATEWAY dev eth0
 
 exit 0
