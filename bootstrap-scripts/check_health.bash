555e3582248fc6b9474f75dd6f78fabaea4a02a9
Aaron Lefkowitz <aaron.lefkowitz@hpe.com>
2015-10-21 11:37:52 -0700

    Adjust job names for the roles for service reg.
    - This could be prone to failure depending on how/how often cloud
      controller worker numbers change.

--- bootstrap-scripts/check_health.bash
+++ bootstrap-scripts/check_health.bash
@@ -9,14 +9,16 @@ job_names="$@"
 
 is_unhealthy=0
 
+monit_status=$(curl -s -u "${monit_user}:${monit_pass}" "http://${monit_addr}:${monit_port}/_status?format=xml")
+if [[ "$?" != 0 ]]; then
+  echo "failed to reach monit at: http://${monit_addr}:${monit_port} using provided username and password"
+  exit 2
+fi
+
+echo -n "$(date '+%Y-%m-%d %H:%M:%S')"
 for service_name in ${job_names}; do
-  monit_status=$(curl -s -u "${monit_user}:${monit_pass}" "http://${monit_addr}:${monit_port}/_status?format=xml")
-  if [[ "$?" != 0 ]]; then
-    echo "failed to reach monit at: http://${monit_addr}:${monit_port} using provided username and password"
-    exit 2
-  fi
   service_health=$(echo "${monit_status}" | xmlstarlet sel -t -m "monit/service[name='${service_name}']" -v status)
-  echo "${service_name} => ${service_health}"
+  echo -n " ${service_name}=${service_health}"
   if [[ 0 != "${service_health}" ]]; then
     is_unhealthy=2
   fi
