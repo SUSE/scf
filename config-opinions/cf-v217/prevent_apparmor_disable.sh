37dcc876539e6d1b85db394f8ce77332ca5e2b78
Eric Promislow <eric.promislow@hpe.com>
2015-10-26 12:35:48 -0700

    This was causing startup to fail on subsequent container restarts where the file had already been moved.

--- config-opinions/cf-v217/prevent_apparmor_disable.sh
+++ config-opinions/cf-v217/prevent_apparmor_disable.sh
@@ -3,4 +3,7 @@
 # runner attempts to disable apparmor which ends up disabling it on the host
 # preventing other containers from starting. It checks for this init script
 # before doing so, so we move it to a different location.
-mv /etc/init.d/apparmor /etc/init.d/apparmor_host
+
+if [[ -f /etc/init.d/apparmor ]] ; then
+    mv /etc/init.d/apparmor /etc/init.d/apparmor_host
+fi
