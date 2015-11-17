c9302145713331523e83d12f6a5150312a539014
Vlad Iovanov <vlad.iovanov@hp.com>
2015-11-16 16:57:26 -0800

    Allow setting local_ip to something else

--- bin/run.sh
+++ bin/run.sh
@@ -15,7 +15,7 @@ fissile dev create-images
 consul_image=($(fissile dev list-roles | grep 'consul'))
 other_images=($(fissile dev list-roles | grep -v 'consul\|smoke_tests\|acceptance_tests'))
 
-local_ip=`${ROOT}/bootstrap-scripts/get_ip`
+local_ip="${local_ip:-`${ROOT}/bootstrap-scripts/get_ip`}"
 store_dir=$HCF_RUN_STORE
 log_dir=$HCF_RUN_LOG_DIRECTORY
 consul_address="http://${local_ip}:8501"
