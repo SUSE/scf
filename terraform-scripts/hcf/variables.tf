367f5facd59b27e5ac3f3dbd15c8b71f347d1658
Eric Promislow <eric.promislow@hpe.com>
2015-12-01 10:00:53 -0800

    HCF-319 - Need a var for the alternate registry-host and the main.
    This is because the core images may reside in one docker registry,
    and the ones in the new version on another.

--- terraform-scripts/hcf/variables.tf
+++ terraform-scripts/hcf/variables.tf
@@ -2,6 +2,10 @@ variable "registry_host" {
 	default = "15.126.242.125:5000"
 }
 
+variable "main_registry_host" {
+	default = "15.126.242.125:5000"
+}
+
 variable "cluster-prefix" {
 	description = "Prefix prepended to all cluster resources (volumes, hostnames, security groups)"
 	default = "hcf"
