540608a616e0b9c3573705f4180dab9fe8a45259
vlad.iovanov <vlad.iovanov@uhurusoftware.com>
2015-12-01 11:20:32 -0800

    Split docker configuration from installation

--- bin/docker/install_docker.sh
+++ bin/docker/install_docker.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 set -e
 
-# Usage: install_docker.sh <IP_ADDRESS> <REGISTRY_ADDRESS> <USER>
-ip_address=$1
-registry_host=$2
-user=$3
+# Usage: install_docker.sh <USER>
+# <USER> defaults to "vagrant" if unset
+user=${1:-vagrant}
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y wget quota
 
-curl -sSL https://experimental.docker.com/ | sh
+curl -sSL https://test.docker.com/ | sh
 
 sudo usermod -aG docker $user
-# allow us to pull from the docker registry
-# TODO: this needs to be removed when we publish to Docker Hub
-
-echo DOCKER_OPTS=\"--cluster-store=etcd://${ip_address}:3379 --cluster-advertise=${ip_address}:2376 --label=com.docker.network.driver.overlay.bind_interface=eth0 --insecure-registry=${registry_host} -H=${ip_address}:2376 -H=unix:///var/run/docker.sock -s=devicemapper -g=/data/docker \" | sudo tee -a /etc/default/docker
-
-# enable cgroup memory and swap accounting
-sudo sed -idockerbak 's/GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX=\"cgroup_enable=memory swapaccount=1\"/' /etc/default/grub
-sudo update-grub
-
-sudo sed -idockerbak 's/local-filesystems and net-device-up IFACE!=lo/local-filesystems and net-device-up IFACE!=lo and started etcd/' /etc/init/docker.conf
--- bin/docker/install_kernel.sh
+++ bin/docker/install_kernel.sh
@@ -2,7 +2,10 @@
 set -e
 
 echo "Update Ubuntu"
-sudo apt-get update 
+sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get dselect-upgrade -y
 echo "Install a kernel with quota support"
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y linux-generic-lts-vivid linux-image-extra-virtual-lts-vivid
+
+sudo reboot now
+sleep 60
