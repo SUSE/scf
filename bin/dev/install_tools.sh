b7b8d426543125ee71a5845cad8dee9229d5d41f
Vlad Iovanov <vlad.iovanov@hpe.com>
2015-12-09 12:18:01 -0800

    Bump fissile build

--- bin/dev/install_tools.sh
+++ bin/dev/install_tools.sh
@@ -6,7 +6,7 @@ bin_dir="${bin_dir:-/home/vagrant/bin}"
 tools_dir="${tools_dir:-/home/vagrant/tools}"
 ubuntu_image="${ubuntu_image:-ubuntu:14.04}"
 configgin_url="${configgin_url:-https://region-b.geo-1.objects.hpcloudsvc.com/v1/54026737306152/configgin/configgin-1.0.1.16_develop%2Fconfiggin-1.0.1.16_develop-linux-x86_64.tgz}"
-fissile_url="${fissile_url:-https://region-b.geo-1.objects.hpcloudsvc.com/v1/54026737306152/fissile-artifacts/fissile-1.0.1.34_develop%2Fbuild%2Flinux-amd64%2Ffissile}"
+fissile_url="${fissile_url:-https://region-b.geo-1.objects.hpcloudsvc.com/v1/54026737306152/fissile-artifacts/fissile-0.11.0.43_develop%2Fbuild%2Flinux-amd64%2Ffissile}"
 gato_url="${gato_url:-https://region-b.geo-1.objects.hpcloudsvc.com/v1/54026737306152/gato/gato-1.0.1.14-develop%2Fbuild%2Fgato}"
 cf_url="${cf_url:-https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.14.0&source=github-rel}"
 
