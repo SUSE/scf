3183a3ce6b9ee41bb65facb507ade1e0d62474cf
Eric Promislow <eric.promislow@hpe.com>
2015-12-14 14:24:41 -0800

    HCF-320 - upgrade changes:
    * No need to check if gato is being executed remotely. Gato now does that.
    
    * Replace upgrade-versions.rb with the Go executable.

--- upgrades/upgrade-217-222.sh
+++ upgrades/upgrade-217-222.sh
@@ -2,19 +2,13 @@
 
 set -e
 
-IP=$1 ; shift
+IP=$1
 if [ -z "$IP" ] ; then
   echo "Usage: $0 A.B.C.D where we're targeting node A.B.C.D"
   exit 1
 fi
 
 # init gato
-
-gato=$(ssh ubuntu@$IP cat /opt/hcf/bin/gato)
-if [[ $gato =~ "-i -t" ]] ; then
-  echo "Remove the -t on /opt/hcf/bin/gato on $IP"
-  exit 1
-fi  
 
 ssh ubuntu@$IP /opt/hcf/bin/gato api http://hcf-consul-server.hcf:8501
 
@@ -45,9 +39,8 @@ ssh ubuntu@$IP bash ./upgrade-v222.bash $IP
 # Get ready to upgrade
 
 scp ../bootstrap-config/role-dependencies.yml ${IP}:
-scp upgrade-versions.rb ubuntu@${IP}:
-
-ssh ubuntu@$IP sudo apt-get install -y ruby
+# scp upgrade-versions.rb ubuntu@${IP}:
+scp $HOME/lab/gostuff/src/hcfupgrade/hcfupgrade ubuntu@${IP}:
 
 # Restarting docker kills the registry, so we're going to first
 # stop all the docker nodes, then add the --insecure-registry setting
@@ -55,13 +48,8 @@ ssh ubuntu@$IP sudo apt-get install -y ruby
 # stopped containers.  Yes, this means hcf gets shut down during an upgrade :(
 
 echo 'On the server, run
-ruby upgrade-versions.rb -t 222 -g latest-epdev -r 15.125.71.0:5000 -d ~/role-dependencies.yml 
 
-or
-
-
-ruby -rdebug upgrade-versions.rb -t 222 -g latest-epdev -r 15.125.71.0:5000 -d ~/role-dependencies.yml 
-
+./hcfupgrade -role-dependencies=$HOME/role-dependencies.yml -target-repository=15.125.71.0:5000 -target-tag="latest" -target-version=222
 '
 
 echo 'Also possible:
