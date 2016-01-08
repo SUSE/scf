32d2b367a83ec92e7fc75bb86d5f2911e2ff5db6
Eric Promislow <eric.promislow@hpe.com>
2016-01-08 12:26:18 -0800

    HCF-320 - Move the upgrades code to hcf-config-{diff,apply}

--- upgrades/cf-run.bash
+++
@@ -1,7 +0,0 @@
-
-cf api api.$1.xip.io --skip-ssl-validation
-cf login -u admin -p fakepassword
-cf create-org demo
-cf target -o demo
-cf create-space space1 -o demo
-cf target -o demo -s space1
--- upgrades/config/analyze/spec/fixtures/hcf-cf-v222/outputs.tf
+++
@@ -1,5 +0,0 @@
-# © Copyright 2015 Hewlett Packard Enterprise Development LP
-
-output "api_endpoint" {
-    value = "https://api.${template_file.domain.rendered}"
-}
