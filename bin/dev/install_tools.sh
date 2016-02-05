b00ee5e82bddd19b0541c725583def78dfbaab2e
vlad.iovanov <vlad.iovanov@uhurusoftware.com>
2016-02-05 11:44:28 -0800

    HCF-458 Update fissile and decrease default worker count

--- bin/dev/install_tools.sh
+++ bin/dev/install_tools.sh
@@ -6,7 +6,7 @@ bin_dir="${bin_dir:-/home/vagrant/bin}"
 tools_dir="${tools_dir:-/home/vagrant/tools}"
 ubuntu_image="${ubuntu_image:-ubuntu:14.04}"
 configgin_url="${configgin_url:-https://region-b.geo-1.objects.hpcloudsvc.com/v1/54026737306152/hcf-ci-concourse/configgin-0.11.0%2B5-g86d9d64.develop-linux-x86_64.tgz}"
-fissile_url="${fissile_url:-https://region-b.geo-1.objects.hpcloudsvc.com/v1/54026737306152/hcf-ci-concourse/fissile-0.11.0%2B94.ge016820.HEAD-linux.amd64.tgz}"
+fissile_url="${fissile_url:-https://region-b.geo-1.objects.hpcloudsvc.com/v1/54026737306152/hcf-ci-concourse/fissile-0.11.0%2B102.g6b9f566.HEAD-linux.amd64.tgz}"
 gato_url="${gato_url:-https://region-b.geo-1.objects.hpcloudsvc.com/v1/54026737306152/hcf-ci-concourse/gato-0.11.0%2B35.gd8840f5.HEAD-linux.amd64.tgz}"
 cf_url="${cf_url:-https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.14.0&source=github-rel}"
 
