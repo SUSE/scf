3631542a433c7df9d2c436e99f09de359c0bdb52
Eric Promislow <eric.promislow@hpe.com>
2016-02-10 14:21:10 -0800

    HCF-488 - Work around error in diego-release/scripts/update

--- bin/init-host-for-vagrant.sh
+++ bin/init-host-for-vagrant.sh
@@ -3,6 +3,10 @@
 # This script pulls in the various cf-*-release submodules so the
 # VM can use them.
 
+function has_upstream() {
+    git rev-parse @{u} > /dev/null 2>&1
+}
+
 ROOT=$(dirname $(dirname $(readlink -f $0)))
 cd ${ROOT}
 cd src
@@ -11,6 +15,19 @@ for dir in * ; do
   # `git submodule update --init --recursive` failed sometimes - no idea why
   git submodule init
   git submodule update --recursive
-  test -x scripts/update && bash -ex scripts/update
+  case $dir in
+      diego-release)
+	  # Deal with upstream error https://github.com/cloudfoundry-incubator/diego-release#132
+	  has_upstream && git pull
+	  if [[ "$(git --version | grep 'version 1.7')x" != "x" ]]; then
+	      git submodule foreach --recursive git submodule sync && git submodule update --init --recursive
+	  else
+	      git submodule sync --recursive && git submodule foreach --recursive git submodule sync  && git submodule update --init --recursive
+	  fi
+	  ;;
+      *)
+	  test -x scripts/update && bash -ex scripts/update
+	  ;;
+  esac
   cd ..
 done
