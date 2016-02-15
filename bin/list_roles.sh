21bd2e0f52ad19db2d8f2f8e648eba895df2f7e2
Andreas Kupries <andreas.kupries@hpe.com>
2016-02-15 11:48:33 -0800

    Additional dev helper scripts to start/stop specific roles, and list the roles.

---
+++ bin/list_roles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+ROOT=`readlink -f "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"`
+
+. "${ROOT}/bin/.fissilerc"
+. "${ROOT}/bin/.runrc"
+. "${ROOT}/container-host-files/opt/hcf/bin/common.sh"
+
+for r in $(get_role_images  | to_roles | sort)
+do
+    echo -e "\t$r"
+done
+
+exit 0
