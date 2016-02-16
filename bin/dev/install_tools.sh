fdde5f2d7698ecf801b6a9c9a92aa1cab6748382
vlad.iovanov <vlad.iovanov@uhurusoftware.com>
2016-02-15 19:51:45 -0800

    hcf-501 Cleanup opinions and update configgin

--- bin/dev/install_tools.sh
+++ bin/dev/install_tools.sh
@@ -5,8 +5,8 @@ set -e
 bin_dir="${bin_dir:-/home/vagrant/bin}"
 tools_dir="${tools_dir:-/home/vagrant/tools}"
 ubuntu_image="${ubuntu_image:-ubuntu:14.04}"
-configgin_url="${configgin_url:-https://region-b.geo-1.objects.hpcloudsvc.com/v1/54026737306152/hcf-ci-concourse/configgin-0.11.0%2B5-g86d9d64.develop-linux-x86_64.tgz}"
-fissile_url="${fissile_url:-https://api.mpce.hpelabs.net:8080/v1/AUTH_7b52c1fb73ad4568bbf5e90bead84e21/hcf-ci-concourse-fissile/fissile-0.11.0%2B131.g0c1c16f.HEAD-linux.amd64.tgz}"
+configgin_url="${configgin_url:-https://15.184.137.5:8080/v1/AUTH_7b52c1fb73ad4568bbf5e90bead84e21/hcf-ci-concourse-configgin/configgin-0.12.0%2B12.gea70828.HEAD-linux-amd64.tgz}"
+fissile_url="${fissile_url:-https://15.184.137.5:8080/v1/AUTH_7b52c1fb73ad4568bbf5e90bead84e21/hcf-ci-concourse-fissile/fissile-0.11.0%2B106.g9e1a994.HEAD-linux.amd64.tgz}"
 gato_url="${gato_url:-https://region-b.geo-1.objects.hpcloudsvc.com/v1/54026737306152/hcf-ci-concourse/gato-0.11.0%2B35.gd8840f5.HEAD-linux.amd64.tgz}"
 cf_url="${cf_url:-https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.14.0&source=github-rel}"
 
@@ -14,7 +14,7 @@ mkdir -p $bin_dir
 mkdir -p $tools_dir
 
 echo "Fetching configgin ..."
-wget -q "$configgin_url" -O $tools_dir/configgin.tgz
+wget --no-check-certificate -q "$configgin_url" -O $tools_dir/configgin.tgz
 echo "Fetching cf CLI ..."
 wget -q "$cf_url"        -O $tools_dir/cf.tgz
 echo "Fetching fissile ..."
