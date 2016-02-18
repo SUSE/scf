a5349565214af5b82ec22087c6a08fab378fc29c
Andreas Kupries <andreas.kupries@hpe.com>
2016-02-18 12:22:51 -0800

    [HCF-504] Updated and expanded to match the changes in the role-manifest spec.

--- bin/rm2ucp.rb
+++ bin/rm2ucp.rb
@@ -17,9 +17,23 @@ def get_roles(path)
   # the_roles.roles[].configuration.variables[].name	/string
   # the_roles.roles[].configuration.variables[].default	/string
   # the_roles.roles[].configuration.templates.<any>	/string
+  # the_roles.roles[].run.capabilities[]		/string
+  # the_roles.roles[].run.persistent-volumes[].path	/string, mountpoint
+  # the_roles.roles[].run.persistent-volumes[].size	/float [GB]
+  # the_roles.roles[].run.shared-volumes[].path		/string, mountpoint
+  # the_roles.roles[].run.shared-volumes[].size		/float [GB]
+  # the_roles.roles[].run.shared-volumes[].tag		/string
+  # the_roles.roles[].run.memory			/float [MB]
+  # the_roles.roles[].run.virtual-cpus			/int
+  # the_roles.roles[].run.exposed-ports[].name		/string
+  # the_roles.roles[].run.exposed-ports[].protocol	/string
+  # the_roles.roles[].run.exposed-ports[].source	/int
+  # the_roles.roles[].run.exposed-ports[].target	/int
+  # the_roles.roles[].run.exposed-ports[].public	/bool
+  # the_roles.roles[].run.hosts.<any>			/string (name -> ip-addr)
   # the_roles.configuration.variables[].name		/string
   # the_roles.configuration.variables[].default		/string
-  # the_roles.configuration.templates.<any>		/string
+  # the_roles.configuration.templates.<any>		/string (key -> value)
 end
 
 def add_parameters(component, variables)
@@ -38,7 +52,90 @@ def add_parameters(component, variables)
       'secret'      => false
     }
 
-    para.push the_para
+    para.push(the_para)
+  end
+end
+
+def add_volumes(fs, component, volumes)
+  vols = component['volume_mounts']
+  serial = 0
+
+  volumes.each do |v|
+    vmount = v['path']
+    if v['tag']
+      # Shared volume, already collected
+      vname = v['tag']
+    else
+      # Private volume, export now
+      vname = 'V$' + component['name'] + '$' + serial.to_s
+      vsize = v['size'] # [GB], same as used by UCP, no conversion required
+      serial += 1
+
+      add_filesystem(fs, vname, vsize, false)
+    end
+
+    the_vol = {
+      'volume_name' => vname,
+      'mount_point' => vmount
+    }
+
+    vols.push(the_vol)
+  end
+end
+
+def convert_port(port)
+  {
+    'name'        => port['name'],
+    'protocol'    => port['protocol'],
+    'source_port' => port['source'],
+    'target_port' => port['target'],
+    'public'      => port['public']
+  }
+end
+
+def add_ports(component, ports)
+  cports = component['service_ports']
+  ports.each do |port|
+    cports.push(convert_port(port))
+  end
+end
+
+def add_filesystem(fs, name, size, shared)
+  the_fs = {
+    'name'       => name,
+    'size_gb'    => size,
+    'filesystem' => 'ext4',
+    'shared'     => shared
+  }
+  fs.push(the_fs)
+end
+
+def abort_on_mismatch(shared, vname, vsize)
+  if shared[vname] && vsize != shared[vname]
+    # Raise error about definition mismatch
+    raise 'Size mismatch for shared volume "' + vname + '": ' +
+      vsize + ', previously ' + shared[vname]
+  end
+end
+
+def collect_shared_filesystems(roles)
+  shared = {}
+  roles.each do |role|
+    runtime = role['run']
+    next unless runtime['shared-volumes']
+    runtime['shared-volumes'].each do |v|
+      vname = v['tag']
+      vsize = v['size']
+      abort_on_mismatch(shared, vname, vsize)
+      shared[vname] = vsize
+    end
+  end
+  shared
+end
+
+def save_shared_filesystems(fs, shared)
+  shared.each do |vname, vsize|
+    add_filesystem(fs, vname, vsize, true)
   end
 end
 
@@ -52,10 +149,17 @@ def roles_to_ucp(roles)
   }
 
   comp = the_ucp['components']
+  fs   = the_ucp['volumes']
+ 
+  save_shared_filesystems(fs, collect_shared_filesystems(roles['roles']))
+
+  # Phase II. Generate UCP data per-role.
   roles['roles'].each do |role|
     rname = role['name']
     ename = rname # TODO: construct proper external name
     iname = rname # TODO: construct proper image name
+
+    runtime = role['run']
 
     the_comp = {
       'name'          => rname,
@@ -63,20 +167,26 @@ def roles_to_ucp(roles)
       'vendor'        => 'HPE',	  # See also toplevel vendor
       'external_name' => ename,
       'image'         => iname,
-      'min_RAM_mb'    => 128,	  # Pulled out of thin air
-      'min_disk_gb'   => 1,	  # Ditto
-      'min_VCPU'      => 1,
+      'min_RAM_mb'    => runtime['memory'],
+      'min_disk_gb'   => 1,	  		# Out of thin air
+      'min_VCPU'      => runtime['virtual-cpus'],
       'platform'      => 'linux-x86_64',
-      'capabilities'  => ['ALL'], # Might be role-specific (privileged vs not)
+      'capabilities'  => runtime['capabilities'],
       'depends_on'    => [],	  # No dependency info in the RM
       'affinity'      => [],	  # No affinity info in the RM
       'labels'        => [rname], # TODO: Maybe also label with the jobs ?
       'min_instances' => 1,
       'max_instances' => 1,
-      'service_ports' => [],	  # This might require role-specific alteration
-      'volume_mounts' => [],
-      'parameters'    => []	  # Fill from role configuration, see below
+      'service_ports' => [],	# Fill from role runtime config, see below
+      'volume_mounts' => [],	# Ditto
+      'parameters'    => []	# Fill from role configuration, see below
     }
+
+    # Record persistent and shared volumes, ports
+    add_volumes(fs, the_comp, runtime['persistent-volumes']) if runtime['persistent-volumes']
+    add_volumes(fs, the_comp, runtime['shared-volumes']) if runtime['shared-volumes']
+
+    add_ports(the_comp, runtime['exposed-ports']) if runtime['exposed-ports']
 
     # Global parameters
     if roles['configuration'] && roles['configuration']['variables']
@@ -91,7 +201,7 @@ def roles_to_ucp(roles)
     # TODO: Should check that the intersection of between global and
     # role parameters is empty.
 
-    comp.push the_comp
+    comp.push(the_comp)
   end
 
   the_ucp
