d53b9959e7efba9d43adbbc31e1fbfc3104ef5be
Vlad Iovanov <viovanov@hotmail.com>
2016-02-21 19:00:06 -0800

    hcf-501 Bump fissile url and no longer install gato

--- bin/dev/install_tools.sh
+++ bin/dev/install_tools.sh
@@ -5,29 +5,25 @@ set -e
 bin_dir="${bin_dir:-/home/vagrant/bin}"
 tools_dir="${tools_dir:-/home/vagrant/tools}"
 ubuntu_image="${ubuntu_image:-ubuntu:14.04}"
-configgin_url="${configgin_url:-https://region-b.geo-1.objects.hpcloudsvc.com/v1/54026737306152/aaa-swift-client-test/configgin-0.12.0%2B14.ge8cf926.develop-linux-amd64.tgz}"
-fissile_url="${fissile_url:-https://api.mpce.hpelabs.net:8080/v1/AUTH_7b52c1fb73ad4568bbf5e90bead84e21/hcf-ci-concourse-fissile/fissile-0.12.0%2B146.g0571438.HEAD-linux.amd64.tgz}"
-gato_url="${gato_url:-https://region-b.geo-1.objects.hpcloudsvc.com/v1/54026737306152/hcf-ci-concourse/gato-0.11.0%2B35.gd8840f5.HEAD-linux.amd64.tgz}"
+configgin_url="${configgin_url:-https://api.mpce.hpelabs.net:8080/v1/AUTH_7b52c1fb73ad4568bbf5e90bead84e21/hcf-ci-concourse-configgin/configgin-0.12.0%2B16.g40f386a.HEAD-linux-amd64.tgz}"
+fissile_url="${fissile_url:-https://api.mpce.hpelabs.net:8080/v1/AUTH_7b52c1fb73ad4568bbf5e90bead84e21/hcf-ci-concourse-fissile/fissile-0.12.0%2B148.g756d190.HEAD-linux.amd64.tgz}"
 cf_url="${cf_url:-https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.14.0&source=github-rel}"
 
 mkdir -p $bin_dir
 mkdir -p $tools_dir
 
 echo "Fetching configgin ..."
-wget --no-check-certificate -q "$configgin_url" -O $tools_dir/configgin.tgz
+wget -q "$configgin_url" -O $tools_dir/configgin.tgz
 echo "Fetching cf CLI ..."
 wget -q "$cf_url"        -O $tools_dir/cf.tgz
 echo "Fetching fissile ..."
-wget --no-check-certificate -q "$fissile_url"   -O - | tar xz -C $bin_dir fissile
-echo "Fetching gato ..."
-wget -q "$gato_url"      -O - | tar xz -C $bin_dir gato
+wget -q "$fissile_url"   -O - | tar xz -C $bin_dir fissile
 
 echo "Unpacking cf CLI ..."
 tar -xzf $tools_dir/cf.tgz -C $bin_dir
 
 echo "Making binaries executable ..."
 chmod +x $bin_dir/fissile
-chmod +x $bin_dir/gato
 chmod +x $bin_dir/cf
 
 echo "Pulling base image ..."
