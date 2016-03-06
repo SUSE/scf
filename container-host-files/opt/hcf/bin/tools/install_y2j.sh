4883583ba93b626093c5cfd8d33717ad6f2bf299
Vlad Iovanov <vlad.iovanov@hpe.com>
2016-03-06 01:52:33 -0800

    HCF-530 Install jq from backports

--- container-host-files/opt/hcf/bin/tools/install_y2j.sh
+++ container-host-files/opt/hcf/bin/tools/install_y2j.sh
@@ -2,5 +2,6 @@
 set -e
 echo 'deb http://archive.ubuntu.com/ubuntu trusty-backports main restricted universe multiverse' | sudo tee -a /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get install -y jq
+sudo apt-get install -y jq/trusty-backports
+
 docker run --rm wildducktheories/y2j y2j.sh installer /usr/local/bin | sudo bash
