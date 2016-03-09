97f5f1eeed12e8266706d720f5349b7dbc32d7cc
Vlad Iovanov <vlad.iovanov@hpe.com>
2016-03-09 12:41:13 -0800

    Bump configgin

--- bin/dev/install_tools.sh
+++ bin/dev/install_tools.sh
@@ -5,7 +5,7 @@ set -e
 bin_dir="${bin_dir:-/home/vagrant/bin}"
 tools_dir="${tools_dir:-/home/vagrant/tools}"
 ubuntu_image="${ubuntu_image:-ubuntu:14.04}"
-configgin_url="${configgin_url:-https://api.mpce.hpelabs.net:8080/v1/AUTH_7b52c1fb73ad4568bbf5e90bead84e21/hcf-ci-concourse-configgin/configgin-0.12.0%2B16.g40f386a.HEAD-linux-amd64.tgz}"
+configgin_url="${configgin_url:-https://api.mpce.hpelabs.net:8080/v1/AUTH_7b52c1fb73ad4568bbf5e90bead84e21/hcf-ci-concourse-configgin/configgin-0.12.0%2B18.gcba7e70.develop-linux-amd64.tgz}"
 fissile_url="${fissile_url:-https://api.mpce.hpelabs.net:8080/v1/AUTH_7b52c1fb73ad4568bbf5e90bead84e21/hcf-ci-concourse-fissile/fissile-0.12.0%2B152.g9f86514.HEAD-linux.amd64.tgz}"
 cf_url="${cf_url:-https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.14.0&source=github-rel}"
 
