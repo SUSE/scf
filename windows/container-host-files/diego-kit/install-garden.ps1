7fe71aa9cfd8becfa29968d86e7a6ff7836a06fa
Stefan Schneider <stefan.schneider@hpe.com>
2016-03-09 19:57:44 +0200

    Use the new diego installer

--- windows/container-host-files/diego-kit/install-garden.ps1
+++ windows/container-host-files/diego-kit/install-garden.ps1
@@ -16,14 +16,35 @@ Function Get-TempPassword() {
 
 ## Create working directory
 
-$wd="C:\diego-kit"
+$wd="C:\garden-kit"
 mkdir -f $wd
 cd $wd
 
 
+## Download and install global dependencies
+
+# VC redist are used by cf-iis8-buildpack
+echo "Downloading VC 2013 and 2015 Update 1 redistributable"
+mkdir -Force "$wd\vc2013", "$wd\vc2015u1"
+curl -Verbose -UseBasicParsing  -OutFile "vc2013\vcredist_x86.exe"  http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x86.exe
+curl -Verbose -UseBasicParsing  -OutFile "vc2013\vcredist_x64.exe"  http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x64.exe
+curl -Verbose -UseBasicParsing  -OutFile "vc2015u1\VC_redist.x86.exe"  https://download.microsoft.com/download/C/E/5/CE514EAE-78A8-4381-86E8-29108D78DBD4/VC_redist.x86.exe
+curl -Verbose -UseBasicParsing  -OutFile "vc2015u1\VC_redist.x64.exe"  https://download.microsoft.com/download/C/E/5/CE514EAE-78A8-4381-86E8-29108D78DBD4/VC_redist.x64.exe
+
+echo "Installing VC 2013 and 2015 Update 1 redistributable"
+start -Wait "vc2013\vcredist_x86.exe"  -ArgumentList "/install /passive /norestart"
+start -Wait "vc2013\vcredist_x64.exe"  -ArgumentList "/install /passive /norestart"
+start -Wait "vc2015u1\VC_redist.x86.exe"  -ArgumentList "/install /passive /norestart"
+start -Wait "vc2015u1\VC_redist.x64.exe"  -ArgumentList "/install /passive /norestart"
+
+echo "Installing Windows Features"
+Install-WindowsFeature  Web-Webserver, Web-WebSockets, AS-Web-Support, AS-NET-Framework, Web-WHC, Web-ASP
+Install-WindowsFeature  Web-Net-Ext, Web-AppInit # Extra features for the cf-iis8-buildpack
+
+
 ## Download installers
 
-$gardenVersion = "v0.107"
+$gardenVersion = "v0.116"
 echo "Downloading GardenWindows.msi $gardenVersion"
 curl  -UseBasicParsing  -Verbose  -OutFile $wd\GardenWindows.msi  https://github.com/cloudfoundry/garden-windows-release/releases/download/$gardenVersion/GardenWindows.msi
 
@@ -71,10 +92,10 @@ cmd /c "$wd\install-garden.bat"
 ## Checking health
 
 echo "Checking Containerizer health"
-echo (curl -UseBasicParsing http://127.0.0.1:1788/api/ping).Content
+echo (curl -UseBasicParsing http://127.0.0.1:1788/api/ping).StatusDescription
 
 echo "Checking Garden health"
-echo (curl -UseBasicParsing http://127.0.0.1:9241/ping).StatusCode
+echo (curl -UseBasicParsing http://127.0.0.1:9241/ping).StatusDescription
 
 echo "Interogating Garden capacity endpoint"
 echo (curl -UseBasicParsing http://127.0.0.1:9241/capacity).Content | ConvertFrom-Json
