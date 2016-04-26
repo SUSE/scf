e694b605d5cf931ee7c1949002fb03e80172e185
Vlad Iovanov <vlad.iovanov@hpe.com>
2016-04-26 12:27:35 -0700

    Add patch for using the hostname as part of the diego cell id

---
+++ container-host-files/etc/hcf/config/fix_diego_cell_id.sh
@@ -0,0 +1,30 @@
+set -e
+
+PATCH_DIR="/var/vcap/jobs-src/rep/templates"
+SENTINEL="${PATCH_DIR}/${0##*/}.sentinel"
+
+if [ -f "${SENTINEL}" ]; then
+  exit 0
+fi
+
+read -d '' cell_patch <<"PATCH" || true
+--- rep_ctl.erb
++++ rep_ctl_patched.erb
+@@ -108,7 +108,7 @@ case $1 in
+       -listenAddr=<%= p("diego.rep.listen_addr") %> \
+       <%= p("diego.rep.preloaded_rootfses").map { |rootfs| "-preloadedRootFS #{rootfs}" }.join(" ") %> \
+       <%= p("diego.rep.rootfs_providers").map { |provider| "-rootFSProvider #{provider}" }.join(" ") %> \
+-      -cellID=<%= spec.job.name %>-<%= spec.index %> \
++      -cellID=<%= spec.job.name %>-<%= spec.index %>-`hostname` \
+       -zone=<%= p("diego.rep.zone") %> \
+       -pollingInterval=<%= "#{p("diego.rep.polling_interval_in_seconds")}s" %> \
+       -evacuationPollingInterval=<%= "#{p("diego.rep.evacuation_polling_interval_in_seconds")}s" %> \
+PATCH
+
+cd "$PATCH_DIR"
+
+echo -e "${cell_patch}" | patch --force
+
+touch "${SENTINEL}"
+
+exit 0
