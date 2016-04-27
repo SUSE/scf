96811fb4a0de28808560988c18d7dc0da6ffa752
Mark Yen <mook@hpe.com>
2016-04-27 15:13:00 -0700

    HCF-622 Update fissile binary to work with cf-release v236
    This has https://github.com/hpcloud/fissile/pull/127 merged in, creating
    the `vcap` user in the compilation container.

--- bin/dev/install_tools.sh
+++ bin/dev/install_tools.sh
@@ -6,7 +6,7 @@ bin_dir="${bin_dir:-/home/vagrant/bin}"
 tools_dir="${tools_dir:-/home/vagrant/tools}"
 ubuntu_image="${ubuntu_image:-ubuntu:14.04}"
 configgin_url="${configgin_url:-https://s3.amazonaws.com/helion-developers/viovanov/hcf/configgin-0.12.0%2B5.g65289be.develop-linux-amd64.tgz}"
-fissile_url="${fissile_url:-https://s3.amazonaws.com/helion-developers/viovanov/hcf/fissile-0.12.0%2B165.gb0a48f6.HEAD-linux.amd64.tgz}"
+fissile_url="${fissile_url:-https://s3.amazonaws.com/helion-developers/marky/hcf/fissile-0.13.1%2B18.g5dde06a.develop-linux.amd64.tgz}"
 cf_url="${cf_url:-https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.14.0&source=github-rel}"
 
 mkdir -p $bin_dir
