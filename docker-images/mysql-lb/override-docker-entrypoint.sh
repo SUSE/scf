17336948eaae14f617b18b9ef6c92ce1b4b5c384
Mark Yen <mook@hpe.com>
2016-05-18 08:59:39 -0700

    HCF-627 Fix mysql-lb haproxy configuration
    - mysql-proxy (switchboard) health check is tcp connection only, no http
    - we can't hard-code .hcf on the hosts, that's not true outside vagrant
    
    Addresses https://github.com/hpcloud/hcf/pull/348#issuecomment-219187822

--- docker-images/mysql-lb/override-docker-entrypoint.sh
+++ docker-images/mysql-lb/override-docker-entrypoint.sh
@@ -4,13 +4,13 @@
 
 CFG=/usr/local/etc/haproxy/haproxy.cfg
 if [[ -n "${MYSQL_PROXY_HOST}" ]] ; then
-    echo "  server ${MYSQL_PROXY_HOST}  ${MYSQL_PROXY_HOST}.hcf:3306 check port 1936" >> $CFG
+    echo "  server ${MYSQL_PROXY_HOST}  ${MYSQL_PROXY_HOST}:3306 check port 1936" >> $CFG
 fi
 if [[ -n "${MYSQL_PROXY_HA_HOST_1}" ]] ; then
-    echo "  server ${MYSQL_PROXY_HA_HOST_1} ${MYSQL_PROXY_HA_HOST_1}.hcf:3306 backup check port 1936" >> $CFG
+    echo "  server ${MYSQL_PROXY_HA_HOST_1} ${MYSQL_PROXY_HA_HOST_1}:3306 backup check port 1936" >> $CFG
 fi
 if [[ -n "${MYSQL_PROXY_HA_HOST_2}" ]] ; then
-    echo "  server ${MYSQL_PROXY_HA_HOST_2} ${MYSQL_PROXY_HA_HOST_2}.hcf:3306 backup check port 1936" >> $CFG
+    echo "  server ${MYSQL_PROXY_HA_HOST_2} ${MYSQL_PROXY_HA_HOST_2}:3306 backup check port 1936" >> $CFG
 fi
 
 # And now call the regular haproxy wrapper
