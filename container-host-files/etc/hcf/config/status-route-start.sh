e3e6c11a08d5b11c674a731b056765ab9e86d0fc
William Gautier <william.gautier@hpe.com>
2016-06-09 16:00:44 -0700

    HCF-676 Replace wget with in-repo archive

--- container-host-files/etc/hcf/config/status-route-start.sh
+++ container-host-files/etc/hcf/config/status-route-start.sh
@@ -4,7 +4,7 @@
 ./container-host-files/opt/hcf/bin/docker/install_docker.sh root
 ./container-host-files/opt/hcf/bin/tools/install_y2j.sh
 
-wget https://github.com/yudai/gotty/releases/download/v0.0.13/gotty_linux_amd64.tar.gz -O - | tar -xzC /usr/bin
+tar -xzf ./container-host-files/etc/hcf/lib/gotty_linux_amd64.tar.gz -C /usr/bin
 
 # Monit doesn't have /usr/local/bin in its PATH
 ln -s /usr/local/bin/y2j /usr/bin/y2j
