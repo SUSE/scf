eaebc58bc949e374cfb5645f0c6f37b7e8f95dc2
William Gautier <william.gautier@hpe.com>
2016-06-08 17:24:14 -0700

    HCF-676 Fix issue using monit, gotty and watch all together
    For some reason using all three causes gotty to display
    “Error opening terminal: unknown.”
    Removing watch fixes this, now we'll need to refresh the browser page
    which isn't too bad

--- container-host-files/etc/hcf/config/status-route-start.sh
+++ container-host-files/etc/hcf/config/status-route-start.sh
@@ -6,11 +6,14 @@
 
 wget https://github.com/yudai/gotty/releases/download/v0.0.13/gotty_linux_amd64.tar.gz -O - | tar -xzC /usr/bin
 
+# Monit doesn't have /usr/local/bin in its PATH
+ln -s /usr/local/bin/y2j /usr/bin/y2j
+
 # Monitor the gotty process (It won't show up in hcf-status, but it'll still restart on crash)
 cat <<EOF > /var/vcap/monit/gotty.monitrc
 check process gotty
   matching "/usr/bin/gotty"
-  start program "/usr/bin/nohup /usr/bin/gotty --port 9050 watch --color /container-host-files/opt/hcf/bin/hcf-status"
+  start program "/usr/bin/nohup /usr/bin/gotty --port 9050 /container-host-files/opt/hcf/bin/hcf-status"
   stop program "/usr/bin/killall /usr/bin/gotty"
   group vcap/var/run
 EOF
