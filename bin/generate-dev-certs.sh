a355eef7bac9abec55d9644d35cc764b73e012a0
Alex Harford <alex.harford@hpe.com>
2016-07-07 13:47:24 -0700

    HCF-721 Simplify path to internal certs dir

--- bin/generate-dev-certs.sh
+++ bin/generate-dev-certs.sh
@@ -40,10 +40,6 @@ BINDIR=`readlink -f "$( cd "$( dirname "${BASH_SOURCE[0]}" )/../container-host-f
 certs_path="/tmp/hcf/certs"
 hcf_certs_path="${certs_path}/hcf"
 internal_certs_dir="${certs_path}/internal"
-bbs_certs_dir="${certs_path}/internal"
-etcd_certs_dir="${certs_path}/internal"
-consul_path="${certs_path}/internal"
-sso_routing_path="${certs_path}/internal"
 output_path="$(readlink --canonicalize-missing "${output_path}")"
 
 # prepare directories
@@ -66,48 +62,48 @@ openssl genrsa -out "${certs_path}/jwt_signing.pem" -passout pass:"${signing_key
 openssl rsa -in "${certs_path}/jwt_signing.pem" -outform PEM -passin pass:"${signing_key_passphrase}" -pubout -out "${certs_path}/jwt_signing.pub"
 
 # Generate internal CA
-certstrap --depot-path "${bbs_certs_dir}" init --common-name "internalCA" --passphrase "${signing_key_passphrase}" --years 10
+certstrap --depot-path "${internal_certs_dir}" init --common-name "internalCA" --passphrase "${signing_key_passphrase}" --years 10
 
 # generate BBS certs (Instructions from https://github.com/cloudfoundry-incubator/diego-release#generating-tls-certificates)
-certstrap --depot-path "${bbs_certs_dir}" request-cert --common-name "bbsServer"  --domain "*.diego-database.hcf,diego-database.hcf,diego-database,*.diego-database,*.diego-database-int.hcf,diego-database-int.hcf,diego-database-int,*.diego-database-int"  --passphrase ""
-certstrap --depot-path "${bbs_certs_dir}"  sign bbsServer  --CA internalCA  --passphrase "${signing_key_passphrase}"
+certstrap --depot-path "${internal_certs_dir}" request-cert --common-name "bbsServer"  --domain "*.diego-database.hcf,diego-database.hcf,diego-database,*.diego-database,*.diego-database-int.hcf,diego-database-int.hcf,diego-database-int,*.diego-database-int"  --passphrase ""
+certstrap --depot-path "${internal_certs_dir}"  sign bbsServer  --CA internalCA  --passphrase "${signing_key_passphrase}"
 
-certstrap --depot-path "${bbs_certs_dir}"  request-cert  --common-name "bbsClient"  --passphrase ""
-certstrap --depot-path "${bbs_certs_dir}"  sign bbsClient  --CA internalCA  --passphrase "${signing_key_passphrase}"
+certstrap --depot-path "${internal_certs_dir}"  request-cert  --common-name "bbsClient"  --passphrase ""
+certstrap --depot-path "${internal_certs_dir}"  sign bbsClient  --CA internalCA  --passphrase "${signing_key_passphrase}"
 
 
 # generate SSO routing certs
-mkdir -p ${sso_routing_path}
-certstrap --depot-path "${sso_routing_path}" request-cert --common-name hcf-sso.hcf --domain "hcf-sso,hcf-sso.hcf" --passphrase ""
-certstrap --depot-path "${sso_routing_path}" sign hcf-sso.hcf --CA internalCA --passphrase "${signing_key_passphrase}"
-cat ${sso_routing_path}/hcf-sso.hcf.crt ${sso_routing_path}/hcf-sso.hcf.key > ${sso_routing_path}/sso_routing.key
-cp ${sso_routing_path}/hcf-sso.hcf.crt ${sso_routing_path}/sso_routing.crt
+mkdir -p ${internal_certs_dir}
+certstrap --depot-path "${internal_certs_dir}" request-cert --common-name hcf-sso.hcf --domain "hcf-sso,hcf-sso.hcf" --passphrase ""
+certstrap --depot-path "${internal_certs_dir}" sign hcf-sso.hcf --CA internalCA --passphrase "${signing_key_passphrase}"
+cat ${internal_certs_dir}/hcf-sso.hcf.crt ${internal_certs_dir}/hcf-sso.hcf.key > ${internal_certs_dir}/sso_routing.key
+cp ${internal_certs_dir}/hcf-sso.hcf.crt ${internal_certs_dir}/sso_routing.crt
 
 # generate ETCD certs (Instructions from https://github.com/cloudfoundry-incubator/diego-release#generating-tls-certificates)
-certstrap --depot-path "${etcd_certs_dir}"  request-cert --common-name "etcdServer"  --domain "*.diego-database.hcf,diego-database.hcf,diego-database,*.diego-database,*.diego-database-int.hcf,diego-database-int.hcf,diego-database-int,*.diego-database-int"  --passphrase ""
-certstrap --depot-path "${etcd_certs_dir}"  sign etcdServer  --CA internalCA  --passphrase "${signing_key_passphrase}"
+certstrap --depot-path "${internal_certs_dir}"  request-cert --common-name "etcdServer"  --domain "*.diego-database.hcf,diego-database.hcf,diego-database,*.diego-database,*.diego-database-int.hcf,diego-database-int.hcf,diego-database-int,*.diego-database-int"  --passphrase ""
+certstrap --depot-path "${internal_certs_dir}"  sign etcdServer  --CA internalCA  --passphrase "${signing_key_passphrase}"
 
-certstrap --depot-path "${etcd_certs_dir}"  request-cert  --common-name "etcdClient"  --passphrase ""
-certstrap --depot-path "${etcd_certs_dir}"  sign etcdClient  --CA internalCA  --passphrase "${signing_key_passphrase}"
+certstrap --depot-path "${internal_certs_dir}"  request-cert  --common-name "etcdClient"  --passphrase ""
+certstrap --depot-path "${internal_certs_dir}"  sign etcdClient  --CA internalCA  --passphrase "${signing_key_passphrase}"
 
-certstrap --depot-path "${etcd_certs_dir}"  request-cert --common-name "etcdPeer"  --domain "*.diego-database.hcf,diego-database.hcf,diego-database,*.diego-database,*.diego-database-int.hcf,diego-database-int.hcf,diego-database-int,*.diego-database-int"  --passphrase ""
-certstrap --depot-path "${etcd_certs_dir}"  sign etcdPeer  --CA internalCA  --passphrase "${signing_key_passphrase}"
+certstrap --depot-path "${internal_certs_dir}"  request-cert --common-name "etcdPeer"  --domain "*.diego-database.hcf,diego-database.hcf,diego-database,*.diego-database,*.diego-database-int.hcf,diego-database-int.hcf,diego-database-int,*.diego-database-int"  --passphrase ""
+certstrap --depot-path "${internal_certs_dir}"  sign etcdPeer  --CA internalCA  --passphrase "${signing_key_passphrase}"
 
 # generate Consul certs (Instructions from https://github.com/cloudfoundry-incubator/consul-release#generating-keys-and-certificates)
 # Server certificate to share across the consul cluster
 server_cn=server.dc1.hcf
-certstrap --depot-path ${consul_path} request-cert --passphrase '' --common-name ${server_cn}
-certstrap --depot-path ${consul_path} sign ${server_cn} --CA internalCA --passphrase "${signing_key_passphrase}"
-mv -f ${consul_path}/${server_cn}.key ${consul_path}/server.key
-mv -f ${consul_path}/${server_cn}.csr ${consul_path}/server.csr
-mv -f ${consul_path}/${server_cn}.crt ${consul_path}/server.crt
+certstrap --depot-path ${internal_certs_dir} request-cert --passphrase '' --common-name ${server_cn}
+certstrap --depot-path ${internal_certs_dir} sign ${server_cn} --CA internalCA --passphrase "${signing_key_passphrase}"
+mv -f ${internal_certs_dir}/${server_cn}.key ${internal_certs_dir}/server.key
+mv -f ${internal_certs_dir}/${server_cn}.csr ${internal_certs_dir}/server.csr
+mv -f ${internal_certs_dir}/${server_cn}.crt ${internal_certs_dir}/server.crt
 
 # Agent certificate to distribute to jobs that access consul
-certstrap --depot-path ${consul_path} request-cert --passphrase '' --common-name 'consul agent'
-certstrap --depot-path ${consul_path} sign consul_agent --CA internalCA --passphrase "${signing_key_passphrase}"
-mv -f ${consul_path}/consul_agent.key ${consul_path}/agent.key
-mv -f ${consul_path}/consul_agent.csr ${consul_path}/agent.csr
-mv -f ${consul_path}/consul_agent.crt ${consul_path}/agent.crt
+certstrap --depot-path ${internal_certs_dir} request-cert --passphrase '' --common-name 'consul agent'
+certstrap --depot-path ${internal_certs_dir} sign consul_agent --CA internalCA --passphrase "${signing_key_passphrase}"
+mv -f ${internal_certs_dir}/consul_agent.key ${internal_certs_dir}/agent.key
+mv -f ${internal_certs_dir}/consul_agent.csr ${internal_certs_dir}/agent.csr
+mv -f ${internal_certs_dir}/consul_agent.crt ${internal_certs_dir}/agent.crt
 
 # generate SSH Host certs
 ssh-keygen -b 4096 -t rsa -f "${certs_path}/ssh_key" -q -N "" -C hcf-ssh-key
@@ -134,17 +130,17 @@ openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 \
 CERTS_ROOT_CHAIN_PEM=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${certs_path}/hcf/hcf.pem")
 JWT_SIGNING_PEM=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${certs_path}/jwt_signing.pem")
 JWT_SIGNING_PUB=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${certs_path}/jwt_signing.pub")
-INTERNAL_CA_CERT=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${sso_routing_path}/internalCA.crt")
-ETCD_PEER_KEY=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${etcd_certs_dir}/etcdPeer.key")
-ETCD_PEER_CRT=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${etcd_certs_dir}/etcdPeer.crt")
-ETCD_SERVER_KEY=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${etcd_certs_dir}/etcdServer.key")
-ETCD_CLIENT_KEY=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${etcd_certs_dir}/etcdClient.key")
-ETCD_SERVER_CRT=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${etcd_certs_dir}/etcdServer.crt")
-ETCD_CLIENT_CRT=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${etcd_certs_dir}/etcdClient.crt")
-BBS_SERVER_KEY=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${bbs_certs_dir}/bbsServer.key")
-BBS_CLIENT_KEY=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${bbs_certs_dir}/bbsClient.key")
-BBS_SERVER_CRT=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${bbs_certs_dir}/bbsServer.crt")
-BBS_CLIENT_CRT=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${bbs_certs_dir}/bbsClient.crt")
+INTERNAL_CA_CERT=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${internal_certs_dir}/internalCA.crt")
+ETCD_PEER_KEY=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${internal_certs_dir}/etcdPeer.key")
+ETCD_PEER_CRT=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${internal_certs_dir}/etcdPeer.crt")
+ETCD_SERVER_KEY=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${internal_certs_dir}/etcdServer.key")
+ETCD_CLIENT_KEY=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${internal_certs_dir}/etcdClient.key")
+ETCD_SERVER_CRT=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${internal_certs_dir}/etcdServer.crt")
+ETCD_CLIENT_CRT=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${internal_certs_dir}/etcdClient.crt")
+BBS_SERVER_KEY=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${internal_certs_dir}/bbsServer.key")
+BBS_CLIENT_KEY=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${internal_certs_dir}/bbsClient.key")
+BBS_SERVER_CRT=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${internal_certs_dir}/bbsServer.crt")
+BBS_CLIENT_CRT=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${internal_certs_dir}/bbsClient.crt")
 SSH_KEY="$(sed '$!{:a;N;s/\n/\\n/;ta}' "${certs_path}/ssh_key")"
 UAA_PRIVATE_KEY=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${uaa_server_key}")
 UAA_CERTIFICATE=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${certs_path}/uaa_ca.crt")
@@ -152,12 +148,12 @@ ROUTER_SSL_CERT=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${certs_path}/router_ssl.cert")
 ROUTER_SSL_KEY=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${certs_path}/router_ssl.key")
 BLOBSTORE_TLS_CERT=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${certs_path}/blobstore_tls.cert")
 BLOBSTORE_TLS_KEY=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${certs_path}/blobstore_tls.key")
-CONSUL_AGENT_CERT=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${consul_path}/agent.crt")
-CONSUL_AGENT_KEY=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${consul_path}/agent.key")
-CONSUL_SERVER_CERT=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${consul_path}/server.crt")
-CONSUL_SERVER_KEY=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${consul_path}/server.key")
-SSO_ROUTE_CERT=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${sso_routing_path}/sso_routing.crt")
-SSO_ROUTE_KEY=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${sso_routing_path}/sso_routing.key")
+CONSUL_AGENT_CERT=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${internal_certs_dir}/agent.crt")
+CONSUL_AGENT_KEY=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${internal_certs_dir}/agent.key")
+CONSUL_SERVER_CERT=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${internal_certs_dir}/server.crt")
+CONSUL_SERVER_KEY=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${internal_certs_dir}/server.key")
+SSO_ROUTE_CERT=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${internal_certs_dir}/sso_routing.crt")
+SSO_ROUTE_KEY=$(sed '$!{:a;N;s/\n/\\n/;ta}' "${internal_certs_dir}/sso_routing.key")
 
 APP_SSH_HOST_KEY_FINGERPRINT=${app_ssh_host_key_fingerprint}
 
