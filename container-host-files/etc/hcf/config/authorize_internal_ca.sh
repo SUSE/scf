7516fc7c91e363cc70b396f6aa6042262fc32954
Alex Harford <alex.harford@hpe.com>
2016-07-07 10:33:03 -0700

    HCF-721 Rename router cert setup script

--- container-host-files/etc/hcf/config/authorize_internal_ca.sh
+++ container-host-files/etc/hcf/config/authorize_internal_ca.sh
@@ -3,5 +3,5 @@
 set -o errexit -o nounset
 
 echo -e ${UAA_CERTIFICATE} > /usr/local/share/ca-certificates/uaa.crt
-echo -e ${SSO_ROUTE_CA_CERT} > /usr/local/share/ca-certificates/sso.crt
+echo -e ${INTERNAL_CA_CERT} > /usr/local/share/ca-certificates/internalCA.crt
 update-ca-certificates
