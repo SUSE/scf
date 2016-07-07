c8e1a1faca451441ae17eaa20146d8ec92901100
Alex Harford <alex.harford@hpe.com>
2016-07-07 13:40:49 -0700

    HCF-721 UAA now uses internal CA

--- container-host-files/etc/hcf/config/authorize_internal_ca.sh
+++ container-host-files/etc/hcf/config/authorize_internal_ca.sh
@@ -2,6 +2,5 @@
 
 set -o errexit -o nounset
 
-echo -e ${UAA_CERTIFICATE} > /usr/local/share/ca-certificates/uaa.crt
 echo -e ${INTERNAL_CA_CERT} > /usr/local/share/ca-certificates/internalCA.crt
 update-ca-certificates
