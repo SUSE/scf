dd7ae02a96cb184918b44d9848540b290a8f1b9e
Jan Dubois <jan.dubois@hpe.com>
2016-07-07 12:10:04 -0700

    HCF-750 Update etcd_ctl.erb patch
    Parts of the script have been moved to a pre-start script, so the old
    patch didn't apply correctly anymore.

--- container-host-files/etc/hcf/config/fix_etcd_bootstrap.sh
+++ container-host-files/etc/hcf/config/fix_etcd_bootstrap.sh
@@ -8,13 +8,13 @@ if [ -f "${SENTINEL}" ]; then
 fi
 
 read -r -d '' setup_patch_etcd_ctl <<'PATCH' || true
---- etcd_ctl.erb.orig   2016-05-12 16:29:44.819321371 +0000
-+++ etcd_ctl.erb        2016-05-12 17:01:32.111257799 +0000
-@@ -59,7 +59,19 @@
-     # Allowed number of open file descriptors
-     ulimit -n 100000
- 
-+    <% if_p("etcd.bootstrap_node") do |bootstrap_node| %>
+--- etcd_ctl.erb.orig
++++ etcd_ctl.erb
+@@ -55,7 +55,19 @@ function main() {
+
+       create_cert_files
+
++      <% if_p("etcd.bootstrap_node") do |bootstrap_node| %>
 +      prior_member_list=""
 +      <% if bootstrap_node != "#{name.gsub('_', '-')}-#{spec.index}" %>
 +      # If this node is not the bootstrap node, wait until at least the
@@ -24,25 +24,24 @@ read -r -d '' setup_patch_etcd_ctl <<'PATCH' || true
 +        sleep 2
 +      done
 +      <% end %>
-+    <% end.else do %>
-     prior_member_list=$(member_list)
-+    <% end %>
- 
-     if [ -z "${prior_member_list}" ]; then
-       cluster_state=new
-@@ -88,6 +88,11 @@
-       my_id=$(extract_my_id "${prior_member_list}")
-       if [ -z "${my_id}" ]; then
-         member_add
-+      fi
++      <% end.else do %>
+       prior_member_list=$(member_list)
++      <% end %>
+
+       if [ -z "${prior_member_list}" ]; then
+         cluster_state=new
+@@ -72,6 +84,11 @@ function main() {
+         my_id=$(extract_my_id "${prior_member_list}")
+         if [ -z "${my_id}" ]; then
+           member_add
++        fi
 +
-+      # If the node was just added, or the node has been added but isn't started,
-+      # ensure it's in the cluster list
-+      if [[ "${cluster}" != *"${this_node}"* ]]; then
-         cluster="${cluster},${this_node}"
-       fi
-     fi
-PATCH
++        # If the node was just added, or the node has been added but isn't started,
++        # ensure it's in the cluster list
++        if [[ "${cluster}" != *"${this_node}"* ]]; then
+           cluster="${cluster},${this_node}"
+         fi
+       fiPATCH
 
 cd "$PATCH_DIR"
 
