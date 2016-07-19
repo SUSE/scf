b12bce6f1536d14a1fafdedbb5a9a725a8b792b4
Vlad Iovanov <vlad.iovanov@hpe.com>
2016-07-19 14:21:55 -0700

    Regenerate dev certs and fix a couple issues on HCP

--- container-host-files/etc/hcf/config/authorize_internal_ca.sh
+++ container-host-files/etc/hcf/config/authorize_internal_ca.sh
@@ -2,5 +2,9 @@
 
 set -o errexit -o nounset
 
+if [ -r /etc/secrets/internal-ca-cert ]; then
+  INTERNAL_CA_CERT=`cat /etc/secrets/internal-ca-cert`;
+fi
+
 echo -e ${INTERNAL_CA_CERT} > /usr/local/share/ca-certificates/internalCA.crt
 update-ca-certificates
