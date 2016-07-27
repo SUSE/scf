fe7a6b8fdf65d6df70793a70e3d084964ddd109e
Davide Agnello <dagnello@hp.com>
2016-07-27 15:43:35 -0700

    Cert updates for SDL and internal_ca bash script
    1. CONSUL_SERVER_CERT missing server.dc1.hcf hostname in SANs
    2. echo of internal ca cert without quotes does not preserve newlines '\n'

--- container-host-files/etc/hcf/config/authorize_internal_ca.sh
+++ container-host-files/etc/hcf/config/authorize_internal_ca.sh
@@ -6,5 +6,5 @@ if [ -r /etc/secrets/internal-ca-cert ]; then
   INTERNAL_CA_CERT=`cat /etc/secrets/internal-ca-cert`;
 fi
 
-echo -e ${INTERNAL_CA_CERT} > /usr/local/share/ca-certificates/internalCA.crt
+echo -e "${INTERNAL_CA_CERT}" > /usr/local/share/ca-certificates/internalCA.crt
 update-ca-certificates
