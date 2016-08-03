a2e816eb26286d40dccb33381d34c0140d7dcd09
Mark Yen <mook@hpe.com>
2016-08-03 12:39:17 -0700

    HCF-828 Address review comments + linting
    Two complexity / line count related warnings remain, but they are not
    worth fixing at this time; that should happen when the rest of
    rm-transformer gets fixed for lint issues.

--- bin/rm-transformer/diego-cell-count-modifier.rb
+++ bin/rm-transformer/diego-cell-count-modifier.rb
@@ -3,7 +3,7 @@
 require 'json'
 
 if ARGV.length < 2
-  puts "Usage: twentyscale.rb <amount> <sd-file> <sid-file>"
+  puts 'Usage: diego-cell-count-modifier.rb <amount> <sd-file> <sid-file>'
   exit 1
 end
 
@@ -11,55 +11,48 @@ CELL_REGEX = /diego-cell-([0-9]+)/
 GRAPH_REGEX = /garden-graph-([0-9]+)/
 
 def scale_service_definition(sd, amount)
-  STDERR.puts "Deleting diego cells from sd"
-  sd["components"].delete_if do |c|
-    match = CELL_REGEX.match(c["name"])
-    !match.nil? && match[1].to_i >= amount
-  end
-  STDERR.puts "Deleting garden volumes from sd"
-  sd["volumes"].delete_if do |v|
-    match = GRAPH_REGEX.match(v["name"])
-    !match.nil? && match[1].to_i >= amount
+  sd['components'].delete_if do |c|
+    match = CELL_REGEX.match(c['name'])
+    match && match[1].to_i >= amount
   end
 
-  STDERR.puts "Adjusting sd version name"
-  sd["sdl_version"] += "-#{amount}cell"
+  sd['volumes'].delete_if do |v|
+    match = GRAPH_REGEX.match(v['name'])
+    match && match[1].to_i >= amount
+  end
 
-  STDERR.puts "Happily applying hideous HSM hacks"
-  sd["parameters"].push({
-    "name" => "workaround-ca-private-key",
-    "description" => "placeholder",
-    "required" => true,
-    "secret" => true,
-    "default" => nil,
-    "generator" => {
-      "id" => "cacert",
-      "generate" => {
-        "type" => "CACertificate",
-        "value_type" => "private_key"
+  sd['sdl_version'] += "-#{amount}cell"
+
+  sd['parameters'].push(
+    'name' => 'workaround-ca-private-key',
+    'description' => 'placeholder',
+    'required' => true,
+    'secret' => true,
+    'default' => nil,
+    'generator' => {
+      'id' => 'cacert',
+      'generate' => {
+        'type' => 'CACertificate',
+        'value_type' => 'private_key'
       }
     }
-  })
+  )
 
   puts JSON.pretty_generate(sd)
 end
 
 def scale_instance_definition(sid, amount)
-  STDERR.puts "Deleting scaling params from sid"
-  sid["scaling"].delete_if do |s|
-    match = CELL_REGEX.match(s["component"])
-    !match.nil? && match[1].to_i >= amount
+  sid['scaling'].delete_if do |s|
+    match = CELL_REGEX.match(s['component'])
+    match && match[1].to_i >= amount
   end
 
-  STDERR.puts "Adjusting sid version name"
-  sid["sdl_version"] += "-#{amount}cell"
+  sid['sdl_version'] += "-#{amount}cell"
 
-  STDERR.puts "Happily applying hideous HSM hacks"
-  sid["parameters"].push({
-    "name" => "workaround-ca-private-key",
-    "value" => "not-required"
-  })
-  sid["version"] = sid["sdl_version"]
+  sid['parameters'].push('name' => 'workaround-ca-private-key',
+                         'value' => 'not-required')
+
+  sid['version'] = sid['sdl_version']
 
   puts JSON.pretty_generate(sid)
 end
@@ -68,7 +61,7 @@ amount = ARGV.shift.to_i
 file_name_in = ARGV.shift
 
 definition = JSON.parse(File.read(file_name_in))
-if definition.has_key? 'instance_id'
+if definition.key? 'instance_id'
   STDERR.puts "Service Instance:   #{file_name_in}"
   scale_instance_definition definition, amount
 else
