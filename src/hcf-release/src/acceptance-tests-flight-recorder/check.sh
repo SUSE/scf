8c4f3523763187ddbcdc768453a9e3e1327c5813
Mihai Buzgau <mihai.buzgau@hp.com>
2016-08-11 16:38:43 +0300

    run tests and move dev specific settings outside of settings.env

--- src/hcf-release/src/acceptance-tests-flight-recorder/check.sh
+++ src/hcf-release/src/acceptance-tests-flight-recorder/check.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+export TERM=linux
 
 NORMAL='\033[0m'
 RED='\033[1;31m'
 GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 
-SOURCE_FILE=/usr/local/etc/source_to_check.txt
+SOURCE_FILE=/var/vcap/packages/acceptance-tests-flight-recorder/bin/source_to_check.txt
 SOURCE_ITEMS=
 REDIS_PASS_FILE=$REDIS_PASSWORD_FILE
 
@@ -51,11 +52,21 @@ redis_pass=$(cat $REDIS_PASS_FILE)
 
 #check to see if each element in SOURCE_FILE has a key in redis
 #thus, is forwarding logs to this flightrecorder
-for source in $SOURCE_ITEMS; do
-        COLOR=$RED
-        if sourceInRedisKeys $source; then
+for i in `seq 1 $1`;
+        do
+     clear
+          count=0
+          for source in $SOURCE_ITEMS; do
+                COLOR=$RED
+           if sourceInRedisKeys $source; then
                 COLOR=$GREEN
-        fi;
-        echo -e "${COLOR}$source${NORMAL}"
+           else
+                 count=$((count+1))
+           fi;
+     echo -e "${COLOR}$source${NORMAL}"
+   done
+   echo "Number of failing service ${count}"
+   [ $count -eq 0 ] && exit 0
+   sleep 1
 done
-
+exit 1
--- src/hcf-release/src/acceptance-tests-flight-recorder/start.sh
+++
@@ -1,5 +0,0 @@
-/usr/local/bin/redis-server /usr/local/etc/redis/redis.conf &
-sleep 2
-/usr/local/etc/flightrecorder > /dev/null &
-sleep 1
-watch -c /usr/local/etc/check.sh
