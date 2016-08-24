ab0c4e0ed5da7a7fe769e0079565dbd996f74985
Florin Dragos <florin.dragos@hpe.com>
2016-08-24 16:44:33 +0300

    [HCFRO-347] Add proxy support for windows-installer

--- windows/hcp/deploy/setup-networking.ps1
+++ windows/hcp/deploy/setup-networking.ps1
@@ -18,13 +18,104 @@ param (
     [Parameter(Mandatory=$true)]
     [string] $etcdCertFile,
     [Parameter(Mandatory=$true)]
-    [string] $etcdCaFile
+    [string] $etcdCaFile,
+	[Parameter(Mandatory=$false)]
+    [string] $httpProxy = "",
+	[Parameter(Mandatory=$false)]
+    [string] $httpsProxy = "",
+	[Parameter(Mandatory=$false)]
+    [string] $noProxy = ""
 )
 
 $ErrorActionPreference = "Stop"
 
 $wd="$PSScriptRoot\resources\hcf-networking"
 mkdir -f $wd  | Out-Null
+
+if (($httpProxy -ne "") -and ($httpsProxy -ne "")) {
+
+    $hklm64 = [Microsoft.Win32.RegistryKey]::OpenBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, [Microsoft.Win32.RegistryView]::Registry64)
+    $hklm32 = [Microsoft.Win32.RegistryKey]::OpenBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, [Microsoft.Win32.RegistryView]::Registry32)
+
+    ## Disable IE first run wizard
+    ## https://www.petri.com/disable-ie8-ie9-welcome-screen
+    $ieMainRegPath = "Software\Policies\Microsoft\Internet Explorer\Main"
+    $disableWizard = { param ($ieRegKey)
+        $ieMainSettings = $ieRegKey.CreateSubKey($ieMainRegPath, [Microsoft.Win32.RegistryKeyPermissionCheck]::ReadWriteSubTree)
+        $ieMainSettings.SetValue("DisableFirstRunCustomize", 1)
+        $ieMainSettings.Dispose()
+    }
+
+    & $disableWizard $hklm64
+    & $disableWizard $hklm32
+
+
+    ## Parse proxy env
+
+    $proxyServers = ""
+
+    if ($httpProxy -ne "") {
+        $proxyServers += "http=$httpProxy"
+    }
+
+    if ($httpsProxy -ne "") {
+        if ($proxyServers -ne "") { $proxyServers += ";" }
+        $proxyServers += "https=$httpsProxy"
+    }
+
+    $bypassList = ($noProxy -replace ',', ';')
+
+    echo "Setting proxy servers     : $proxyServers"
+    echo "Setting proxy bypass list : $bypassList"
+
+
+    # Disable ProxySettingsPerUser
+    $proxyPolicyRegPath = "Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings"
+    $setProxyPolicy = { param ($ieRegKey)
+        $iePolicySettings = $ieRegKey.CreateSubKey($proxyPolicyRegPath, [Microsoft.Win32.RegistryKeyPermissionCheck]::ReadWriteSubTree)
+        $iePolicySettings.SetValue("ProxySettingsPerUser", 0)
+        $iePolicySettings.Dispose()
+    }
+
+    & $setProxyPolicy $hklm64
+    & $setProxyPolicy $hklm32
+
+    # Set proxy for WinINET at machine level
+    $proxyRegPath = "Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+    $setProxy = { param ($ieRegKey)
+        $ieSettings = $ieRegKey.CreateSubKey($proxyRegPath, [Microsoft.Win32.RegistryKeyPermissionCheck]::ReadWriteSubTree)
+        $ieSettings.SetValue("AutoDetect", 0)
+        $ieSettings.SetValue("ProxyEnable", 1)
+        $ieSettings.SetValue("MigrateProxy", 0)
+        $ieSettings.SetValue("ProxyServer", $proxyServers, [Microsoft.Win32.RegistryValueKind]::String)
+        $ieSettings.SetValue("ProxyOverride", $bypassList)
+        $ieSettings.Dispose()
+    }
+
+    & $setProxy $hklm64
+    & $setProxy $hklm32
+
+    $hklm64.Dispose()
+    $hklm32.Dispose()
+
+    # Set proxy for WinHTTP
+    netsh winhttp set proxy proxy-server="$proxyServers" bypass-list="$bypassList"
+
+    # Run Internet Explorer with an interactive logon token to
+    # initialize the system proxy.
+    # I agree, it is weird, but it is the only workaround that does not require
+    # the user to RDP into the box and initialize IE.
+    $startInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $startInfo.FileName = "powershell.exe"
+    $startInfo.Arguments = "/c", "(new-object -ComObject internetexplorer.application).navigate('127.0.0.1')"
+    $startInfo.RedirectStandardOutput = $true
+    $startInfo.UseShellExecute = $false
+    $startInfo.CreateNoWindow = $false
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $startInfo
+    $process.Start()
+    $process.WaitForExit()
+}
 
 Write-Output "Getting local IP ..."
 $localIP = (Find-NetRoute -RemoteIPAddress $k8MasterIP)[0].IPAddress
--- windows/hcp/deploy/shell.ps1
+++ windows/hcp/deploy/shell.ps1
@@ -8,7 +8,7 @@ echo @"
   .\setup-networking.ps1
  Example:
   .\setup-networking.ps1 -?
-  .\setup-networking.ps1 -k8MasterIP 172.31.22.219 -k8sServSubnet 172.16.0.0/16 -etcdKeyFile C:\net-setup\client.key -etcdCertFile C:\net-setup\client.cert -etcdCaFile C:\net-setup\ca.crt
+  .\setup-networking.ps1 -k8MasterIP 172.31.22.219 -k8sServSubnet 172.16.0.0/16 -etcdKeyFile C:\net-setup\client.key -etcdCertFile C:\net-setup\client.cert -etcdCaFile C:\net-setup\ca.crt -httpProxy http://my-proxy:8080 -httpsProxy http://my-proxy:8080 -noProxy "127.0.0.1,10.*.*.*,172.16.*.*,192.168.*.*"
 
  To install the Cloud Foundry Diego components for Windows run the following script:
   .\install-windows-hcf.ps1
