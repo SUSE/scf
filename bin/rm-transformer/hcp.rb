82994d63304a7962ddf159877829ec18055a0ddd
Andreas Kupries <andreas.kupries@hpe.com>
2016-08-31 10:34:10 -0700

    [HCF-910] Replace regexp processing of the templates with mustache parser and iterating/recursing through the returned array structure.

--- bin/rm-transformer/hcp.rb
+++ bin/rm-transformer/hcp.rb
@@ -405,7 +405,7 @@ class ToHCP < Common
         next if @allpara.has_key? vname
         next if /^HCP_/ =~ vname
 
-        STDERR.puts "Template #{property}: #{vname} referenced, not declared"
+        STDERR.puts "Template \033[0;31m#{property}\033[0m: Referencing undeclared variable \033[0;31m#{vname}\033[0m"
       end
     end
 
@@ -413,38 +413,37 @@ class ToHCP < Common
   end
 
   def parameters_in_template(template)
-    # Locate all of
-    #   ((#name)), ((^name)), ((/name)), ((name))
-    #
-    # Note however, if we see '((={{' look for
-    #   {{#name}}, {{^name}}, {{/name}}, {{name}}
-    #
-    # instead
+    # Note: The prefix "{{=(( ))=}}" is required because role manifest
+    # uses ((, )) as delimiters by default, which is non-default for
+    # mustache. The prefix activates our delimiters.
 
     template = template.to_s
+    tokens = Mustache::Template.new("{{=(( ))=}}" + template).tokens
 
-    if /\(\(=\{\{/.match(template)
-      # Delimiter change. Remove the change instructions and then
-      # parse with the new delimiters.
-      template.gsub!(/\(\(=\{\{/, '')
-      template.gsub!(/\}\}=\)\)/, '')
-      template.gsub!(/\{\{=\(\)/, '')
-      template.gsub!(/\)\)=\}\}/, '')
-      matches = template.scan(/\{\{[^}]*\}\}/)
-    else
-      matches = template.scan(/\(\([^)]*\)\)/)
+    vars = []
+    vars_in_tokens(vars, tokens)
+    vars.uniq
+  end
+
+  def vars_in_tokens(vars,tokens)
+    tokens.each do |atoken|
+      # Skip :multi
+      next unless atoken.kind_of?(Array)
+
+      # Skip static and other non-mustache things
+      next unless atoken[0] == :mustache
+
+      # Now we know that we are looking at a ((...)) form.  All of
+      # them have the primary variable(s) at the same place. Take and
+      # remember them.
+      vars.push(*atoken[2][2])
+      next if atoken[1] == :etag
+
+      # We are now looking at the more complex forms ((#...), ((^...),
+      # and ((/...).  These all have a nested set of tokens we have to
+      # process as well.
+      vars_in_tokens(vars, atoken[4])
     end
-
-    # Clean the matches, remove the delimiters from the pieces.
-    matches.each do |match|
-      match.gsub!(/\{\{/, '')
-      match.gsub!(/\}\}/, '')
-      match.gsub!(/\(\(/, '')
-      match.gsub!(/\)\)/, '')
-      match.gsub!(/^#/, '')
-      match.gsub!(/^[\^]/, '')
-      match.gsub!(/^\//, '')
-    end.uniq
   end
 
   def determine_component_parameters(rolemanifest)
