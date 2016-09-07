f4ff6a5e4b050f83000722d774e6a90e49c6207a
Mihai Buzgau <mihai.buzgau@hp.com>
2016-09-07 16:33:41 +0300

    bump HCP Flight Recorder and remove redis dependency

--- src/hcf-release/src/acceptance-tests-flight-recorder/check.sh
+++ src/hcf-release/src/acceptance-tests-flight-recorder/check.sh
@@ -8,13 +8,7 @@ YELLOW='\033[1;33m'
 
 SOURCE_FILE=/var/vcap/packages/acceptance-tests-flight-recorder/config/source_to_check.txt
 SOURCE_ITEMS=
-REDIS_PASS_FILE=$REDIS_PASSWORD_FILE
-
-#check if redis pass file is set
-if [ -z $REDIS_PASS_FILE ]; then
-	echo "Redis pass is not known"
-	exit 255
-fi
+FLIGHT_RECORDER_LOG_PATH=/var/vcap/sys/log/flight-recorder.log
 
 #check if the source file exists
 function checkSource {
@@ -30,27 +24,19 @@ if ! checkSource ; then
         exit 255
 fi
 
-if [ ! -f $REDIS_PASS_FILE ]; then
-        echo "Could not find $REDIS_PASS_FILE"
-        exit 255
-fi
 
-#iterate through the keys in redis to search for source
-function sourceInRedisKeys {
+#iterate through the logs to search for source
+function sourceInLog {
         SOURCE=$1
-        redis_keys=$(redis-cli -a $redis_pass keys \*)
-
-        for key in $redis_keys; do
-                if [ $key == $SOURCE ]; then
-                        return 0
-                fi
-        done
+	log=`cat /var/vcap/sys/log/flight-recorder.log  | grep "\"hostname\":\"${SOURCE}\"" | wc -l`
+        if [ $log -gt 0 ]; then
+            return 0
+        fi
         return 1
 }
 
-redis_pass=$(cat $REDIS_PASS_FILE)
 
-#check to see if each element in SOURCE_FILE has a key in redis
+#check to see if each element in SOURCE_FILE has a key in flight recorder
 #thus, is forwarding logs to this flightrecorder
 for i in `seq 1 $1`;
         do
@@ -58,7 +44,7 @@ for i in `seq 1 $1`;
           count=0
           for source in $SOURCE_ITEMS; do
                 COLOR=$RED
-           if sourceInRedisKeys $source; then
+           if sourceInLog $source; then
                 COLOR=$GREEN
            else
                  count=$((count+1))
