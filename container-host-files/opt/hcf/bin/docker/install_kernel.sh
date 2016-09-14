7e9b58d3ac72d779008d4e20da4cb3f5117fd924
Jan Dubois <jan.dubois@hpe.com>
2016-09-13 18:10:59 -0700

    HCF-960 Move vagrant box to xenial kernel

--- container-host-files/opt/hcf/bin/docker/install_kernel.sh
+++ container-host-files/opt/hcf/bin/docker/install_kernel.sh
@@ -8,7 +8,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get dselect-upgrade -y
 echo "Install a kernel with quota support"
 sudo apt-get update
 # We have no hard dependency on the version, just tracking HCP
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y linux-generic-lts-wily linux-image-extra-virtual-lts-wily
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y linux-generic-lts-xenial linux-image-generic-lts-xenial
 
 # Load aufs at boot time
 echo "aufs" | sudo tee -a /etc/modules
