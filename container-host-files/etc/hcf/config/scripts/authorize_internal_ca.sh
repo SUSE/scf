1c5b85e42165289837343bdf2ecfcd02ffdc4daf
Vlad Iovanov <vlad.iovanov@hpe.com>
2016-10-03 11:13:52 -0700

    Register CA certs before trying to talk to UAA

--- container-host-files/etc/hcf/config/scripts/authorize_internal_ca.sh
+++ container-host-files/etc/hcf/config/scripts/authorize_internal_ca.sh
@@ -4,8 +4,6 @@
 # - if available, the internal CA used to identify the components in the cluster
 # - if available, the CA used for UAA (from HCP)
 
-set -o errexit -o nounset
-
 if [ -r /etc/secrets/internal-ca-cert ]; then
     cp /etc/secrets/internal-ca-cert /usr/local/share/ca-certificates/internalCA.crt
 elif [ -n "${INTERNAL_CA_CERT:-}" ]; then
