1555e7e4ae397daa8fe1db367cbe6a6f78a300c7
Eric Promislow <eric.promislow@hpe.com>
2016-10-19 17:07:31 -0700

    HCF-974 - Bump cf-mysql-release to v31
    One of our upstream patches was accepted, and a few other properties
    have changed.

--- container-host-files/etc/hcf/config/scripts/patches/fix_mysql_advertise_ip.sh
+++ container-host-files/etc/hcf/config/scripts/patches/fix_mysql_advertise_ip.sh
@@ -10,13 +10,12 @@ fi
 read -r -d '' setup_patch_mariadb_ctl_config <<'PATCH' || true
 --- mariadb_ctl_config.yml.erb	2016-03-15 22:17:31.000000000 +0000
 +++ mariadb_ctl_config_patched.yml.erb	2016-04-23 02:17:12.000000000 +0000
-@@ -31,5 +31,5 @@ Manager:
+@@ -58,4 +58,4 @@ Manager:
    <% cluster_ips.each do |ip| %>
    - <%= ip %>
    <% end %>
--  MyIP: <%= spec.networks.send(p('network_name')).ip %>
-+  MyIP: <%= p('cf_mysql.mysql.advertise_host') || spec.networks.send(p('network_name')).ip %>
-   DatabaseStartupTimeout: <%= (p('cf_mysql.mysql.database_startup_timeout') * 0.8).round %>
+-  MyIP: <%= network_ip %>
++  MyIP: <%= p('cf_mysql.mysql.advertise_host') || network_ip %>
 PATCH
 
 read -r -d '' setup_patch_my_cnf <<'PATCH' || true
@@ -24,10 +23,10 @@ read -r -d '' setup_patch_my_cnf <<'PATCH' || true
 +++ my.cnf_patched.erb	2016-04-23 02:10:44.000000000 +0000
 @@ -28,7 +28,7 @@ nice      = 0
  wsrep_provider=/var/vcap/packages/mariadb/lib/plugin/libgalera_smm.so
- wsrep_provider_options="gcache.size=<%= p('cf_mysql.mysql.gcache_size') %>M;pc.recovery=TRUE"
+ wsrep_provider_options="gcache.size=<%= p('cf_mysql.mysql.gcache_size') %>M;pc.recovery=TRUE;pc.checksum=TRUE"
  wsrep_cluster_address="gcomm://<%= cluster_ips.join(",") %>"
--wsrep_node_address='<%= spec.networks.send(p('network_name')).ip %>'
-+wsrep_node_address='<%= p('cf_mysql.mysql.advertise_host') || spec.networks.send(p('network_name')).ip %>'
+-wsrep_node_address='<%= network_ip %>:<%= p('cf_mysql.mysql.galera_port') %>'
++wsrep_node_address='<%= p('cf_mysql.mysql.advertise_host') || spec.networks.send(p('network_name')).ip %>:<%= p('cf_mysql.mysql.galera_port') %>'
  wsrep_node_name='<%= name %>/<%= index %>'
  wsrep_cluster_name='cf-mariadb-galera-cluster'
  wsrep_sst_method=xtrabackup-v2
