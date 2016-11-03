b000efa6e331f3663d1c341e6142cf7e6a618a7f
Andreas Kupries <andreas.kupries@hpe.com>
2016-11-03 10:43:18 -0700

    [HCF-1057] Update vagrant install_tools to pull the new fissile and configgin.

--- bin/dev/install_tools.sh
+++ bin/dev/install_tools.sh
@@ -5,8 +5,8 @@ set -e
 bin_dir="${bin_dir:-/home/vagrant/bin}"
 tools_dir="${tools_dir:-/home/vagrant/tools}"
 ubuntu_image="${ubuntu_image:-ubuntu:14.04}"
-configgin_url="${configgin_url:-https://concourse-hpe.s3.amazonaws.com/configgin-1.1.0%2B4.g999ac54.develop-linux-amd64.tgz}"
-fissile_url="${fissile_url:-https://concourse-hpe.s3.amazonaws.com/fissile-2.0.2%2B64.g5219a8b.develop-linux.amd64.tgz}"
+configgin_url="${configgin_url:-https://concourse-hpe.s3.amazonaws.com/configgin-1.1.0%2B7.ga6a0f12.develop-linux-amd64.tgz}"
+fissile_url="${fissile_url:-https://concourse-hpe.s3.amazonaws.com/fissile-2.0.2%2B69.g036e756.develop-linux.amd64.tgz}"
 cf_url="${cf_url:-https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.21.1&source=github-rel}"
 
 mkdir -p $bin_dir
