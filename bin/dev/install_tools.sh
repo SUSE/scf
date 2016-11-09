71fca0026305f979a6798fa6de16a4856440904d
Andreas Kupries <andreas.kupries@hpe.com>
2016-11-09 15:23:43 -0800

    [HCF-1105] Install stampy.

--- bin/dev/install_tools.sh
+++ bin/dev/install_tools.sh
@@ -6,6 +6,8 @@ bin_dir="${bin_dir:-/home/vagrant/bin}"
 tools_dir="${tools_dir:-/home/vagrant/tools}"
 fissile_url="${fissile_url:-https://concourse-hpe.s3.amazonaws.com/fissile-2.0.2%2B71.g608c02c.develop-linux.amd64.tgz}"
 cf_url="${cf_url:-https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.21.1&source=github-rel}"
+stampy_url="${sampy_url:-https://concourse-hpe.s3.amazonaws.com/stampy-0.0.0%2B4.gf876033.master-linux.amd64.tgz}"
+
 
 mkdir -p $bin_dir
 mkdir -p $tools_dir
@@ -15,11 +17,15 @@ wget -q "$cf_url"        -O $tools_dir/cf.tgz
 echo "Fetching fissile ..."
 wget -q "$fissile_url"   -O - | tar xz -C $bin_dir fissile
 
+echo "Fetching stampy ..."
+wget -q "$stampy_url"   -O - | tar xz -C $bin_dir stampy
+
 echo "Unpacking cf CLI ..."
 tar -xzf $tools_dir/cf.tgz -C $bin_dir
 
 echo "Making binaries executable ..."
 chmod +x $bin_dir/fissile
+chmod +x $bin_dir/stampy
 chmod +x $bin_dir/cf
 
 echo "Pulling ruby bosh image ..."
