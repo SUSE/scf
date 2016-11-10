91a5fbb742905388e7cadc5b3c8bbbe49eac40d1
Mark Yen <mook@hpe.com>
2016-11-10 11:36:14 -0800

    HCF-1110 Also hard code host names in windows/windows-cell/dev-box/envs.ps1

--- windows/windows-cell/dev-box/envs.ps1
+++ windows/windows-cell/dev-box/envs.ps1
@@ -4,7 +4,6 @@ $coreIpAddress = "192.168.77.77"
 
 $hcfSettings = New-Object System.Collections.Hashtable
 (cat "C:\hcf\bin\settings\settings.env") -split '`n' |  % { $s = $_ -split ('=', 2); $hcfSettings.Add( $s[0], $s[1] ) }
-(cat "C:\hcf\bin\settings\hosts.env") -split '`n' |  % { $s = $_ -split ('=', 2); $hcfSettings.Add( $s[0], $s[1] ) }
 (cat "C:\hcf\bin\settings\certs.env") -split '`n' | % { $s = $_ -split ('=', 2); $hcfSettings.Add( $s[0], $s[1] -replace ( "\\n", "`n") ) }
 
 
@@ -19,7 +18,7 @@ $env:STACKS = "win2012r2;windows2012R2"
 $env:REP_ZONE = "windows"
 $env:REP_MEMORY_MB = "8192" # "auto"
 
-$env:CONSUL_SERVER_IP = $hcfSettings.'CONSUL_HOST'
+$env:CONSUL_SERVER_IP = 'consul-int'
 $env:CONSUL_ENCRYPT_KEY = $hcfSettings.'CONSUL_ENCRYPTION_KEYS'
 $env:CONSUL_CA_CRT = $hcfSettings.'CONSUL_CA_CERT'
 $env:CONSUL_AGENT_CRT = $hcfSettings.'CONSUL_AGENT_CERT'
@@ -28,9 +27,9 @@ $env:CONSUL_AGENT_KEY = $hcfSettings.'CONSUL_AGENT_KEY'
 $env:BBS_CA_CRT = $hcfSettings.'BBS_CA_CRT'
 $env:BBS_CLIENT_CRT = $hcfSettings.'BBS_CLIENT_CRT'
 $env:BBS_CLIENT_KEY = $hcfSettings.'BBS_CLIENT_KEY'
-$env:BBS_ADDRESS = 'https://' + $hcfSettings.'DIEGO_DATABASE_HOST' + ':8889'
+$env:BBS_ADDRESS = 'https://diego-database-int:8889'
 
-$env:ETCD_CLUSTER = 'http://' + $hcfSettings.'ETCD_HOST' + ':4001'
+$env:ETCD_CLUSTER = 'http://etcd-int:4001'
 $env:LOGGRAGATOR_SHARED_SECRET = $hcfSettings.'LOGGREGATOR_SHARED_SECRET'
 $env:LOGGREGATOR_JOB = $env:COMPUTERNAME
 $env:LOGGRAGATOR_INDEX = 0
