2da89ab0adee379ba91b79dc1aacda1648529b01
Andreas Kupries <andreas.kupries@hpe.com>
2016-11-10 12:33:35 -0800

    [HCF-1098] Address comment, better name for temp file, use variable.

--- bin/validation.sh
+++ bin/validation.sh
@@ -4,13 +4,15 @@ set -e
 ROOT=`readlink -f "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"`
 
 stampy ${ROOT}/hcf_metrics.csv "${BASH_SOURCE[0]}" validation start
-
 stampy ${ROOT}/hcf_metrics.csv "${BASH_SOURCE[0]}" validation::show-properties start
-fissile show properties --output yaml > $$
-stampy ${ROOT}/hcf_metrics.csv "${BASH_SOURCE[0]}" validation::show-properties done
 
+PROPS=fissile-properties-$$.yaml
+fissile show properties --output yaml > $PROPS
+
+stampy ${ROOT}/hcf_metrics.csv "${BASH_SOURCE[0]}" validation::show-properties done
 stampy ${ROOT}/hcf_metrics.csv "${BASH_SOURCE[0]}" validation::docker start
-docker < $$ run \
+
+docker < $PROPS run \
     --interactive \
     --rm \
     --volume ${HOME}/.bosh:/root/.bosh \
@@ -19,5 +21,5 @@ docker < $$ run \
     bash -l -c "rbenv global 2.2.3 && ${ROOT}/bin/config-validator.rb"
 stampy ${ROOT}/hcf_metrics.csv "${BASH_SOURCE[0]}" validation::docker done
 
-rm $$
+rm $PROPS
 stampy ${ROOT}/hcf_metrics.csv "${BASH_SOURCE[0]}" validation done
