699b7dd1e2f924494dbd3945a4e1940cdbe9d1f2
Mark Yen <mook@hpe.com>
2017-01-04 17:30:49 -0800

    HCF-1195 Allow customizing blobstore access rules HCF-1196 Fix customizing insecure docker registries

--- container-host-files/opt/hcf/bin/common.sh
+++ container-host-files/opt/hcf/bin/common.sh
@@ -84,11 +84,22 @@ function start_role {
   # Load the map that details which vars are allowed for which role
   role_params=$(cat ${ROOT}/vagrant.json | jq -r ".[\"${role}\"]")
 
+  local edef ename evalue
   local the_env=()
   # Iterate through all env vars that match the ones used by the role
   while read -r edef
   do
-      the_env+=("--env=${edef}")
+      if test "${edef:0:1}" == "#" -o -z "${edef}" ; then
+        # Skip comments
+        continue
+      fi
+      ename="${edef%%=*}"
+      evalue="${edef#*=}"
+      if test "${evalue:0:1}${evalue: -1}" == "''" ; then
+        # The variable starts and ends with a single quote; strip it
+        evalue="${evalue:1:-1}"
+      fi
+      the_env+=("--env=${ename}=${evalue}")
   done < <(echo "${env_file_contents}" | grep -w "${role_params}")
 
   domain_suffix=$(echo "${env_file_contents}" | awk -F= '/^HCP_SERVICE_DOMAIN_SUFFIX=/ { print $2 }')
