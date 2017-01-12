cdf60bcb2e269a83ad6f5f42cdbff329355062da
Alex Harford <alex.harford@hpe.com>
2017-01-12 10:11:16 -0800

    HCF-1208 Add mysql pod recovery script

---
+++ bin/set_mysql_bootstrap_node.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# NOTE: This script needs to be run before upgrading!
+# 
+# MySQL pods do not come up correctly after an upgrade. Ensure they can cluster
+# correctly by stopping the mysqld processes and setting one of the pods as the
+# bootstrap. When the new pods come up, only that one will have the bootstrap
+# setting in its volume.
+
+set -euf -o pipefail
+
+get_mysql_pods() {
+  kubectl get pods --namespace hcf | awk '/^mysql-[0-9]+-/{print $1}'
+}
+
+stop_mysql_processes() {
+  local pod=$1
+  kubectl exec --namespace hcf "${pod}" monit stop all
+}
+
+get_sequences() {
+  local pods=$*
+  local sequences=()
+
+  for pod in ${pods}
+  do
+    sequences+=($(kubectl exec --namespace hcf "${pod}" -- awk '/^seqno/{print $2}' /var/vcap/store/mysql/grastate.dat))
+  done
+
+  echo "${sequences[@]}"
+}
+
+set_pod_as_bootstrap() {
+  local pod=$1
+  kubectl exec --namespace hcf "${pod}" -- sh -c "echo NEEDS_BOOTSTRAP > /var/vcap/store/mysql/state.txt"
+}
+
+main() {
+  local pods=($(get_mysql_pods))
+
+  for pod in "${pods[@]}"
+  do
+    stop_mysql_processes "${pod}"
+  done
+
+  # Once the processes are stopped, the sequences are written to disk  
+  local sequences=($(get_sequences "${pods[@]}"))
+ 
+  # seqno is -1 on a pod that hasn't joined the cluster so start
+  # counting at -2.
+  local latest=-2
+  local latest_index=0
+
+  # Find the pod with the highest sequence number  
+  for idx in $(seq 0 $((${#pods[@]} - 1)))
+  do
+    if [[ "${sequences[idx]}" -gt "${latest}" ]]; then
+      latest="${sequences[idx]}"
+      latest_index="${idx}"
+    fi
+  done
+  
+  echo "Highest sequence: ${latest}"
+  echo "Bootstrap pod: ${pods[${latest_index}]}"
+  
+  set_pod_as_bootstrap "${pods[${latest_index}]}"
+}
+
+main
