b2ac5aa90a387ce3a4f421adcbe082be9c54f346
Andreas Kupries <andreas.kupries@hpe.com>
2017-01-27 14:01:29 -0800

    Addressed comments made by Jan. Simplification of setup/teardown.

--- src/hcf-release/src/acceptance-tests-brain/test-scripts/017_buildpacks.sh
+++ src/hcf-release/src/acceptance-tests-brain/test-scripts/017_buildpacks.sh
@@ -10,26 +10,9 @@ CF_SPACE=${CF_SPACE:-space}-$(random_suffix)
 
 ## # # ## ### Login & standard entity setup/cleanup ### ## # #
 
-function login_cleanup() {
-    trap "" EXIT ERR
-    set +o errexit
-
-    cf delete-space -f ${CF_SPACE}
-    cf delete-org -f ${CF_ORG}
-
-    set -o errexit
-}
-trap login_cleanup EXIT ERR
-
 # target, login, create work org and space
 cf api --skip-ssl-validation api.${CF_DOMAIN}
 cf auth ${CF_USERNAME} ${CF_PASSWORD}
-
-cf create-org ${CF_ORG}
-cf target -o ${CF_ORG}
-
-cf create-space ${CF_SPACE}
-cf target -s ${CF_SPACE}
 
 ## # # ## ### Test-specific configuration ### ## # #
 
@@ -46,7 +29,6 @@ function test_cleanup() {
     rm -rf "${TMP}"
 
     set -o errexit
-    login_cleanup
 }
 trap test_cleanup EXIT ERR
 
