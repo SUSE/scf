fd31cf7d3e4d455bbfb63b97f30c3dbb90d1e12b
Vlad Iovanov <vlad.iovanov@hpe.com>
2017-02-07 10:42:35 -0800

    Fix UAA and nfs issues

--- container-host-files/etc/hcf/config/scripts/load-hcp-env.sh
+++ container-host-files/etc/hcf/config/scripts/load-hcp-env.sh
@@ -5,10 +5,8 @@
 
 # Note that this is *sourced* into run.sh, so we can't exit the shell.
 
-if test -n "${HCP_INSTANCE_ID:-}" ; then
-  # export any UAA clients we need
-  export UAA_CLIENTS=`cat /var/vcap/packages/role-manifest/uaa_clients.json`
+# export any UAA clients we need
+export UAA_CLIENTS=`cat /var/vcap/packages/role-manifest/uaa_clients.json`
 
-  # export any UAA authorities we may need
-  export UAA_USER_AUTHORITIES=`cat /var/vcap/packages/role-manifest/uaa_authorities.json`
-fi
+# export any UAA authorities we may need
+export UAA_USER_AUTHORITIES=`cat /var/vcap/packages/role-manifest/uaa_authorities.json`
--- container-host-files/etc/hcf/config/scripts/perform_nfs_test.sh
+++ container-host-files/etc/hcf/config/scripts/perform_nfs_test.sh
@@ -1,5 +1,6 @@
 set -e
 
+mkdir -p touch /var/vcap/nfs/shared/
 touch /var/vcap/nfs/shared/.nfs_test
 
 exit 0
