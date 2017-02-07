35d2ea192a8398513a51c8b84be11a0016d3b438
Mark Yen <mook@hpe.com>
2017-02-07 11:54:17 -0800

    Fix HA host name lookups for k8s

--- container-host-files/etc/hcf/config/scripts/configure-HA-hosts.sh
+++ container-host-files/etc/hcf/config/scripts/configure-HA-hosts.sh
@@ -13,34 +13,25 @@
 find_cluster_ha_hosts() {
     local component_name="${1}"
 
-    # on Vagrant / AWS ; HA is not supported
-    # Fall back to simple hostname
-
-    if test -z "${HCP_INSTANCE_ID:-}" ; then
-        echo "[\"${component_name}-0.${component_name}-pod\"]"
+    if test -z "${KUBERNETES_SERVICE_HOST:-}" ; then
+        # on Vagrant / AWS ; HA is not supported
+        # Fall back to simple hostname
+        echo "[\"${component_name}-int\"]"
         return 0
     fi
-    #
-    # # We are on HCP, loop over the environment to locate the component
-    # # name variables.
-    #
-    # local hosts=''
-    # local i=0
-    #
-    # while test "${i}" -lt 100 ; do
-    #     local varname="${component_name^^}_${i}_INT_SERVICE_HOST";
-    #     # !varname => Double deref of the variable.
-    #     if test -z "${!varname:-}" ; then
-    #         break
-    #     fi
-    #
-    #     # Note: The varname deref gives us an IP address. We want the
-    #     # actual host name, and construct it.
-    #     hosts="${hosts},\"${component_name}-${i}-int.${HCP_SERVICE_DOMAIN_SUFFIX}\""
-    #     i="$(expr "${i}" + 1)"
-    # done
-    # # Return the result, with [] around the hostnames, removing the leading comma
-    # echo "[${hosts#,}]"
+
+    # We don't have the k8s namespace available, but it _is_ in resolv.conf
+    local domain="$(awk '/^search/ { print $2 }' /etc/resolv.conf)"
+
+    # Loop over the environment to locate the component name variables.
+
+    local hosts=''
+    local name
+    for name in $(dig "${component_name}-pod.${domain}" -t SRV | awk '/IN A/ { print $1 }') ; do
+        hosts="${hosts},\"${name%.}\""
+    done
+    # Return the result, with [] around the hostnames, removing the leading comma
+    echo "[${hosts#,}]"
 }
 
 export CONSUL_HCF_CLUSTER_IPS="$(find_cluster_ha_hosts consul)"
