631818616ac5cd777b59c122e20477ac87b98025
Mark Yen <mook@hpe.com>
2017-02-17 10:39:27 -0800

    `make validate` should not leave temporary files behind
    Also fix issues raised by shellcheck

--- bin/validation.sh
+++ bin/validation.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 set -e
 
-ROOT=`readlink -f "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"`
+ROOT="$(readlink -f "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../")"
 
-stampy ${ROOT}/hcf_metrics.csv "${BASH_SOURCE[0]}" validation start
-stampy ${ROOT}/hcf_metrics.csv "${BASH_SOURCE[0]}" validation::show-properties start
+stampy "${ROOT}/hcf_metrics.csv" "${BASH_SOURCE[0]}" validation start
+stampy "${ROOT}/hcf_metrics.csv" "${BASH_SOURCE[0]}" validation::show-properties start
 
 PROPS=fissile-properties-$$.yaml
+trap "rm '${PROPS}'" EXIT
 fissile show properties --output yaml > ${PROPS}
 
-stampy ${ROOT}/hcf_metrics.csv "${BASH_SOURCE[0]}" validation::show-properties done
-stampy ${ROOT}/hcf_metrics.csv "${BASH_SOURCE[0]}" validation::docker start
+stampy "${ROOT}/hcf_metrics.csv" "${BASH_SOURCE[0]}" validation::show-properties "done"
+stampy "${ROOT}/hcf_metrics.csv" "${BASH_SOURCE[0]}" validation::docker start
 
 docker < ${PROPS} run \
     --interactive \
     --rm \
-    --volume ${HOME}/.bosh:/root/.bosh \
-    --volume $ROOT/:$ROOT/ \
+    --volume "${HOME}/.bosh:/root/.bosh" \
+    --volume "$ROOT/:$ROOT/" \
     helioncf/hcf-pipeline-ruby-bosh \
     bash -l -c "rbenv global 2.2.3 && ${ROOT}/bin/config-validator.rb"
-stampy ${ROOT}/hcf_metrics.csv "${BASH_SOURCE[0]}" validation::docker done
-
-rm ${PROPS}
-stampy ${ROOT}/hcf_metrics.csv "${BASH_SOURCE[0]}" validation done
+stampy "${ROOT}/hcf_metrics.csv" "${BASH_SOURCE[0]}" validation::docker "done"
+stampy "${ROOT}/hcf_metrics.csv" "${BASH_SOURCE[0]}" validation "done"
