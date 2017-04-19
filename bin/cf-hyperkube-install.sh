7b2f0cb9f76f8cd72726d98aa505d287df98b144
Prabal Sharma <prabal.sharma@suse.com>
2017-04-19 10:00:42 -0700

    scf install with one script

--- bin/cf-hyperkube-install.sh
+++ bin/cf-hyperkube-install.sh
@@ -2,8 +2,41 @@
 #!/bin/bash
 set -ex
 
+#install Go
+cd ; curl https://storage.googleapis.com/golang/go1.8.1.linux-amd64.tar.gz | sudo tar -C /usr/local -xz
+export PATH=$PATH:/usr/local/go/bin:/home/vagrant/go/bin
+
+#set gopath
+export GOPATH=/home/vagrant/go/
+
+git clone https://github.com/hpcloud/uaa-fissile-release.git
+
+#install ruby
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:brightbox/ruby-ng
+sudo apt update
+sudo apt-get install ruby2.3
+
+#install gems
+sudo gem install bosh_cli
+sudo gem install bundler
+
+cd ~/uaa-fissile-release
+git submodule update --init --recursive
+
+docker pull ubuntu:14.04 #'fissile build layer compilation' fails without this 
+
+#avoid Gem dependency issue
+sed -i "s/ruby '2\.3\.1'/ruby '~> 2.3'/g" ~/uaa-fissile-release/src/cf-mysql-release/src/cf-mysql-broker/Gemfile
+
+#Build UAA
+go get github.com/square/certstrap
+make build
+make kube-configs
+
 make -C ~/hcf hyperkube
 cd ~/uaa-fissile-release/
+
 #Run UAA
 kubectl create namespace uaa
 kubectl create -n uaa -f kube/bosh/
