36084dd530f7dc0f90853ecf198f59764d4f5e9a
Mark Yen <mark.yen@suse.com>
2017-04-20 17:04:06 -0700

    Fix validation using new hcf-pipeline-ruby-bosh docker image
    We no longer use rbenv (because root installs the rubies)

--- bin/validation.sh
+++ bin/validation.sh
@@ -18,8 +18,9 @@ docker < ${PROPS} run \
     --rm \
     --volume ${FISSILE_CACHE_DIR}:/root/.bosh/cache:ro \
     --volume $ROOT/:$ROOT/:ro \
+    --env RUBY_VERSION=2.2.3 \
     helioncf/hcf-pipeline-ruby-bosh \
-    bash -l -c "rbenv global 2.2.3 && ${ROOT}/bin/config-validator.rb"
+    bash --login -c "${ROOT}/bin/config-validator.rb"
 
 stampy "${ROOT}/hcf_metrics.csv" "${BASH_SOURCE[0]}" validation::docker "done"
 stampy "${ROOT}/hcf_metrics.csv" "${BASH_SOURCE[0]}" validation "done"
