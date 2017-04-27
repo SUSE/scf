cb11868798b723c777d5957adf5444891d82a1a8
Alex Harford <alex.harford@hpe.com>
2017-04-27 11:08:59 -0700

    Update to v9 of the compatibility list

--- bin/get-cf-versions.sh
+++ bin/get-cf-versions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# set -x
+set -x
 
 if [ -z "$1" ]; then
     echo "usage: get-versions RELEASE"
@@ -14,7 +14,7 @@ RELEASE=$1
 CF_RELEASE=https://raw.githubusercontent.com/cloudfoundry/cf-release/master/releases/cf-$RELEASE.yml
 COMMIT_HASH=$(curl $CF_RELEASE 2>/dev/null | yaml2json | jq '"X"+.commit_hash')
 
-COMPAT=https://raw.githubusercontent.com/cloudfoundry/diego-cf-compatibility/master/compatibility-v6.csv
+COMPAT=https://raw.githubusercontent.com/cloudfoundry/diego-cf-compatibility/master/compatibility-v9.csv
 RELEASE_INFO=$(curl $COMPAT 2>/dev/null | perl -pe '$. == 1 or s/,/,X/g' | csv2json | jq -c "map( select(.[\"cf-release-commit-sha\"] | contains($COMMIT_HASH)))|.[-1]")
 
 echo $RELEASE_INFO | jq . | perl -pe 's/"X/"/'
