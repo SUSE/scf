792f6303ca41899c7dda117853600cebf9664d1a
Prabal Sharma <prabal.sharma@suse.com>
2017-05-18 16:39:53 -0700

    cf-hyperkube-install fix

--- bin/cf-hyperkube-install.sh
+++ bin/cf-hyperkube-install.sh
@@ -15,6 +15,7 @@ export GOPATH=/home/vagrant/go/
 git clone https://github.com/hpcloud/uaa-fissile-release.git
 
 #install ruby
+sudo apt-get update
 sudo apt-get install -y software-properties-common
 sudo apt-add-repository -y ppa:brightbox/ruby-ng
 sudo apt update -y
@@ -49,7 +50,6 @@ fissile build packages
 fissile build images
 fissile build kube -k kube/ --use-memory-limits=false \
     -D $(echo env/*.env | tr ' ' ',')
-fissile show image | xargs -i@ docker tag @ "${FISSILE_DOCKER_REGISTRY}/@"
 
 make -C ~/hcf hyperkube	
 
@@ -61,6 +61,7 @@ kubectl create -n uaa -f kube-test/exposed-ports.yml
 #Build CF
 kubectl create namespace cf
 cd ~/hcf
+source .envrc
 make vagrant-prep
 bash bin/settings/kube/ca.sh
 bin/generate-dev-certs.sh cf bin/settings/certs.env
@@ -69,5 +70,3 @@ make kube
 #run cf
 kubectl create -n cf -f ./kube/bosh
 kubectl create -n cf -f ./kube/bosh-task/post-deployment-setup.yml
-kubectl create -n cf -f ./kube/bosh-task/autoscaler-create-service.yml
-kubectl create -n cf -f ./kube/bosh-task/sso-create-service.yml
