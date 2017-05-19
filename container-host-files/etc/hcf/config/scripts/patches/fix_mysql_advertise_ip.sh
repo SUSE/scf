bb935038e880e50dde3df7dbe5c0aad139be76e8
Andreas Kupries <andreas.kupries@suse.com>
2017-05-19 11:01:16 -0700

    patch-work patches undo removal of bulk_api_password - still referenced in/by api-worker and clock-global.

--- container-host-files/etc/hcf/config/scripts/patches/fix_mysql_advertise_ip.sh
+++ container-host-files/etc/hcf/config/scripts/patches/fix_mysql_advertise_ip.sh
@@ -7,37 +7,23 @@ if [ -f "${SENTINEL}" ]; then
   exit 0
 fi
 
-read -r -d '' setup_patch_mariadb_ctl_config <<'PATCH' || true
---- mariadb_ctl_config.yml.erb	2016-03-15 22:17:31.000000000 +0000
-+++ mariadb_ctl_config_patched.yml.erb	2016-04-23 02:17:12.000000000 +0000
-@@ -58,5 +58,5 @@ Manager:
-   <% cluster_ips.each do |ip| %>
-   - <%= ip %>
-   <% end %>
--  MyIP: <%= network_ip %>
-+  MyIP: <%= p('cf_mysql.mysql.advertise_host') || network_ip %>
-   ConnectionTimeout: 600
-PATCH
-
 read -r -d '' setup_patch_my_cnf <<'PATCH' || true
---- my.cnf.erb	2016-03-15 22:17:31.000000000 +0000
-+++ my.cnf_patched.erb	2016-04-23 02:10:44.000000000 +0000
-@@ -28,7 +28,8 @@ nice      = 0
+--- my.cnf.erb	2017-05-17 13:19:47.871198692 -0700
++++ my.cnf_patched.erb	2017-05-19 09:29:03.369987394 -0700
+@@ -47,7 +47,8 @@
+ # GALERA options:
+ wsrep_on=ON
  wsrep_provider=/var/vcap/packages/mariadb/lib/plugin/libgalera_smm.so
 -wsrep_provider_options="gcache.size=<%= p('cf_mysql.mysql.gcache_size') %>M;pc.recovery=TRUE;pc.checksum=TRUE"
-+wsrep_provider_options="gcache.size=<%= p('cf_mysql.mysql.gcache_size') %>M;pc.recovery=TRUE;pc.checksum=TRUE;ist.recv_addr=<%= network_ip %>:4568"
-+wsrep_sst_receive_address='<%= network_ip %>:4444'
++wsrep_provider_options="gcache.size=<%= p('cf_mysql.mysql.gcache_size') %>M;pc.recovery=TRUE;pc.checksum=TRUE;ist.recv_addr=<%= node_host %>:4568"
++wsrep_sst_receive_address='<%= node_host %>:4444'
  wsrep_cluster_address="gcomm://<%= cluster_ips.join(",") %>"
--wsrep_node_address='<%= network_ip %>:<%= p('cf_mysql.mysql.galera_port') %>'
-+wsrep_node_address='<%= p('cf_mysql.mysql.advertise_host') || spec.networks.send(p('network_name')).ip %>:<%= p('cf_mysql.mysql.galera_port') %>'
+ wsrep_node_address='<%= node_host %>:<%= p('cf_mysql.mysql.galera_port') %>'
  wsrep_node_name='<%= name %>/<%= index %>'
- wsrep_cluster_name='cf-mariadb-galera-cluster'
- wsrep_sst_method=xtrabackup-v2
 PATCH
 
 cd "$PATCH_DIR"
 
-echo -e "${setup_patch_mariadb_ctl_config}" | patch --force
 echo -e "${setup_patch_my_cnf}" | patch --force
 
 touch "${SENTINEL}"
