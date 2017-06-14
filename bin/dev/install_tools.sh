a98e117167797e227fda5be4278772c3288c501f
Andreas Kupries <andreas.kupries@suse.com>
2017-06-14 10:25:19 -0700

    Updated to version of fissile with modified handling of secrets

--- bin/dev/install_tools.sh
+++ bin/dev/install_tools.sh
@@ -5,7 +5,7 @@ set -o errexit -o nounset
 s3="https://cf-opensusefs2.s3.amazonaws.com/fissile"
 
 # Tool versions
-thefissile="$(echo "fissile-5.0.0+49.gf5e9403" | sed -e 's/+/%2B/')"
+thefissile="$(echo "fissile-5.0.0+57.gdb657e9" | sed -e 's/+/%2B/')"
 
 # Installs tools needed to build and run HCF
 bin_dir="${bin_dir:-output/bin}"
