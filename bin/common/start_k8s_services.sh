73ba958c5e3fccf4e3773b0cee231a61171143ee
Hart Simha <hart.simha@suse.com>
2017-06-29 09:58:15 -0700

    Start docker before tool installation.
    The certgen installation uses docker

--- bin/common/start_k8s_services.sh
+++ bin/common/start_k8s_services.sh
@@ -4,7 +4,6 @@ set -ex
 mkdir -p /var/lib/kubelet
 mkdir -p /tmp/hostpath_pv
 mkdir -p /home/vagrant/.fissile
-sed -i 's/DOCKER_OPTS="\(.*\)"/DOCKER_OPTS="\1 --storage-driver=overlay2"/' /etc/sysconfig/docker
 systemctl daemon-reload
 systemctl enable etcd kube-apiserver kubelet kube-controller-manager kube-proxy kube-scheduler
-systemctl restart containerd docker etcd kube-apiserver kubelet kube-controller-manager kube-proxy kube-scheduler
+systemctl restart etcd kube-apiserver kubelet kube-controller-manager kube-proxy kube-scheduler
