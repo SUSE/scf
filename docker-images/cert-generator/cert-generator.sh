4233e595fb20058e32ee6545369e05274c34b592
Alex Harford <alex.harford@hpe.com>
2017-07-04 10:09:53 -0700

    Generate yaml files to be used as helm values.yaml

--- docker-images/cert-generator/cert-generator.sh
+++ docker-images/cert-generator/cert-generator.sh
@@ -3,6 +3,9 @@ set -e
 
 export PATH=$PATH:/root/go/bin
 
-/generate-certs.sh -e uaa-settings /out/uaa-certs.env
-/generate-dev-certs.sh -e scf-settings cf /out/scf-certs.env
+/generate-certs.sh -e uaa-settings /tmp/uaa-certs.env > /dev/null
+/generate-dev-certs.sh -e scf-settings ${namespace} /tmp/scf-certs.env > /dev/null
+
+sed 's/^\([A-Z_]\+\)=\(.\+\)/\1: "\2"/g' < /tmp/uaa-certs.env > /out/uaa-cert-values.yaml
+sed 's/^\([A-Z_]\+\)=\(.\+\)/\1: "\2"/g' < /tmp/scf-certs.env > /out/scf-cert-values.yaml
 
