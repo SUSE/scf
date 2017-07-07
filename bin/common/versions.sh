100f46f2a3ec29c9626759fec803855c896282d9
Hart Simha <hart.simha@suse.com>
2017-07-07 11:37:20 -0700

    Add vagrant provisioning scripts

---
+++ bin/common/versions.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+set -o errexit -o nounset
+
+# Versions of the pieces pulled into SCF (outside of submodules).
+# This file is sourced everywhere one or more of the versions are
+# needed.
+
+# Used in: bin/dev/install_tool.sh
+
+export CFCLI_VERSION="6.21.1"
+export FISSILE_VERSION="5.0.0+62.gba2a780"
+export HELM_VERSION="2.4.2"
+export HELM_CERTGEN_VERSION="master"
+export KK_VERSION="40a5b3756cf4bcbed940e6156272c0af"
+export KUBECTL_VERSION="1.5.4"
+export K_VERSION="0.0.12"
+export STAMPY_VERSION="0.0.0.22.gbb93bf3"
+export UBUNTU_VERSION="14.04"
+
+# For stampy we need the major+minor+patch as a separate value.
+export STAMPY_MAJOR=$(echo "$STAMPY_VERSION" | sed -e 's/\.g.*//' -e 's/\.[^.]*$//')
+
+# Notes
+# splatform/bosh-cli - Unversioned docker pull
+
+# Used in: .envrc
+
+export FISSILE_STEMCELL_VERSION=42.2-6.ga651b2d-28.33
+
+# Used in: bin/generate-dev-certs.sh
+
+export GOLANG_VERSION=1.7
+
+# Used in: make/include/versioning
+
+export CF_VERSION=251
+
+# Notes
+# github.com/square/certstrap - Unversioned `go get`
+
+# Show versions, if called on its own.
+# # ## ### ##### ######## ############# #####################
+
+if [ "X$(basename "$0")" = "Xversions.sh" ]
+then
+    echo cf '           =' $CF_VERSION
+    echo cf-cli '       =' $CFCLI_VERSION
+    echo fissile '      =' $FISSILE_VERSION
+    echo go '           =' $GOLANG_VERSION
+    echo helm '         =' $HELM_VERSION
+    echo helm-certgen ' =' $HELM_CERTGEN_VERSION
+    echo k '            =' $K_VERSION
+    echo kk '           =' $KK_VERSION
+    echo kubectl '      =' $KUBECTL_VERSION
+    echo stampy '       =' $STAMPY_VERSION
+    echo stemcell '     =' $FISSILE_STEMCELL_VERSION
+    echo ubuntu '       =' $UBUNTU_VERSION
+    echo
+fi
+
+if id -u vagrant >& /dev/null; then
+  SCF_BIN_DIR="${SCF_BIN_DIR:-/usr/local/bin}"
+  SCF_TOOLS_DIR="${SCF_TOOLS_DIR:-/home/vagrant/tools}"
+else
+  SCF_BIN_DIR="${SCF_BIN_DIR:-output/bin}"
+  SCF_TOOLS_DIR="${SCF_TOOLS_DIR:-output/tools}"
+fi
--- bin/dev/install_tools.sh
+++ bin/dev/install_tools.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -o errexit -o nounset
 
-# Get version information
-. "$(dirname "$0")/versions.sh"
+# Get version information and set destination paths
+. "$(dirname "$0")/../common/versions.sh"
 
 # Tool locations
 s3="https://cf-opensusefs2.s3.amazonaws.com/fissile"
@@ -10,81 +10,40 @@ s3="https://cf-opensusefs2.s3.amazonaws.com/fissile"
 # Tool versions
 thefissile="fissile-$(echo "${FISSILE_VERSION}" | sed -e 's/+/%2B/')"
 
-# Installs tools needed to build and run HCF
-bin_dir="${bin_dir:-output/bin}"
-tools_dir="${tools_dir:-output/tools}"
-ubuntu_image="${ubuntu_image:-ubuntu:${UBUNTU_VERSION}}"
 fissile_url="${fissile_url:-${s3}/${thefissile}.linux-amd64.tgz}"
-cf_url="${cf_url:-https://cli.run.pivotal.io/stable?release=linux64-binary&version=${CFCLI_VERSION}&source=github-rel}"
 stampy_url="${stampy_url:-https://github.com/SUSE/stampy/releases/download/${STAMPY_MAJOR}/stampy-${STAMPY_VERSION}.linux-amd64.tgz}"
-kubectl_url="${kubectl_url:-https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl}"
-k_url="${k_url:-https://github.com/aarondl/kctl/releases/download/v${K_VERSION}/kctl-linux-amd64}"
-kk_url="${kk_url:-https://gist.githubusercontent.com/jandubois/40a5b3756cf4bcbed940e6156272c0af/raw/${KK_VERSION}/kk}"
-helm_url="${helm_url:-https://kubernetes-helm.storage.googleapis.com/helm-v${HELM_VERSION}-linux-amd64.tar.gz}"
 
-mkdir -p "${bin_dir}"
-mkdir -p "${tools_dir}"
+mkdir -p "${SCF_BIN_DIR}"
+mkdir -p "${SCF_TOOLS_DIR}"
 
-bin_dir="$(cd "${bin_dir}" && pwd)"
+SCF_BIN_DIR="$(cd "${SCF_BIN_DIR}" && pwd)"
 
-echo "Fetching cf CLI $cf_url ..."
-wget -q "$cf_url"        -O "${tools_dir}/cf.tgz"
 echo "Fetching fissile $fissile_url ..."
 wget -q "$fissile_url"   -O - | tar xz --to-stdout fissile > "${FISSILE_BINARY}"
-echo "Installed: $("${FISSILE_BINARY}" version)"
 
 echo "Fetching stampy $stampy_url ..."
-wget -q "$stampy_url"   -O - | tar xz -C "${bin_dir}" stampy
-
-echo "Unpacking cf CLI ..."
-tar -xzf "${tools_dir}/cf.tgz" -C "${bin_dir}"
-
-echo "Fetching kubectl ${kubectl_url} ..."
-wget -q "${kubectl_url}" -O "${bin_dir}/kubectl"
-wget -q "${k_url}" -O "${bin_dir}/k"
-wget -q "${kk_url}" -O "${bin_dir}/kk"
-
-echo "Fetching helm from ${helm_url} ..."
-wget -q "${helm_url}" -O - | tar xz -C "${bin_dir}" --strip-components=1 linux-amd64/helm
+wget -q "$stampy_url"   -O - | tar xz -C "${SCF_BIN_DIR}" stampy
 
 echo "Making binaries executable ..."
 chmod a+x "${FISSILE_BINARY}"
-chmod a+x "${bin_dir}/stampy"
-chmod a+x "${bin_dir}/cf"
-chmod a+x "${bin_dir}/kubectl"
-chmod a+x "${bin_dir}/k"
-chmod a+x "${bin_dir}/kk"
-chmod a+x "${bin_dir}/helm"
+chmod a+x "${SCF_BIN_DIR}/stampy"
 
-echo "Installing certstrap ..."
-# We run chown in docker to avoid requiring sudo
-docker run --rm -v "${bin_dir}":/out:rw "golang:${GOLANG_VERSION}" /usr/bin/env GOBIN=/out go get github.com/square/certstrap
-if [[ $(stat -c '%u' "${bin_dir}/certstrap") -eq 0 ]]; then
-  docker run --rm -v "${bin_dir}":/out:rw "golang:${GOLANG_VERSION}" /bin/chown "$(id -u):$(id -g)" /out/certstrap
-fi
+echo "Installed: $("${FISSILE_BINARY}" version)"
 
 echo "Pulling ruby bosh image ..."
 docker pull splatform/bosh-cli
-
-# Note that we might not have a k8s available; do this only if we're in vagrant
-if systemctl is-active kube-apiserver.service ; then
-  echo "Installing tiller for helm ..."
-  helm init
-else
-  echo "Skipping tiller installation for helm; no local kube found"
-fi
 
 echo "Installing helm-certgen ..."
 helm_certgen_dir="$(mktemp -d)"
 trap "rm -rf '${helm_certgen_dir}'" EXIT
 git clone --branch "${HELM_CERTGEN_VERSION}" --depth 1 https://github.com/SUSE/helm-certgen.git "${helm_certgen_dir}"
-docker run --rm -v "${bin_dir}":/out:rw -v "${helm_certgen_dir}:/go/src/github.com/SUSE/helm-certgen:ro" "golang:${GOLANG_VERSION}" /usr/bin/env GOBIN=/out go get github.com/SUSE/helm-certgen
-if [[ $(stat -c '%u' "${bin_dir}/helm-certgen") -eq 0 ]]; then
-  docker run --rm -v "${bin_dir}":/out:rw "golang:${GOLANG_VERSION}" /bin/chown "$(id -u):$(id -g)" /out/helm-certgen
+docker run --rm -v "${SCF_BIN_DIR}":/out:rw -v "${helm_certgen_dir}:/go/src/github.com/SUSE/helm-certgen:ro" "golang:${GOLANG_VERSION}" /usr/bin/env GOBIN=/out go get github.com/SUSE/helm-certgen
+if [[ $(stat -c '%u' "${SCF_BIN_DIR}/helm-certgen") -eq 0 ]]; then
+  docker run --rm -v "${SCF_BIN_DIR}":/out:rw "golang:${GOLANG_VERSION}" /bin/chown "$(id -u):$(id -g)" /out/helm-certgen
 fi
 mkdir -p "${HOME}/.helm/plugins" # Necessary if we didn't run `helm init`
 rm -rf "${HOME}/.helm/plugins/certgen"
 mv --no-target-directory "${helm_certgen_dir}/plugin" "${HOME}/.helm/plugins/certgen"
 rm -rf "${helm_certgen_dir}"
 
-echo "Done."
+echo "Dev-tool installation done."
