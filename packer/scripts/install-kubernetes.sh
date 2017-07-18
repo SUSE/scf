b253737778bbf518e05008f94a82546f3ae79dbc
Hart Simha <hart.simha@suse.com>
2017-07-18 15:12:45 -0700

    Use 17.04 CE docker again

--- packer/scripts/install-kubernetes.sh
+++ packer/scripts/install-kubernetes.sh
@@ -5,24 +5,24 @@
 set -o errexit -o xtrace
 
 swapoff -a
-zypper --non-interactive install --no-confirm docker
-usermod --append --groups docker vagrant
 
 zypper --non-interactive addrepo --gpgcheck --refresh --priority 120 --check \
     obs://Virtualization:containers Virtualization:containers
 zypper --non-interactive --gpg-auto-import-keys refresh
 zypper --non-interactive repos --uri # for troubleshooting
 zypper --non-interactive install --no-confirm --from Virtualization:containers \
-    etcd-3.1.0-11.6 \
-    kubernetes-common-1.6.1-3.3 \
-    kubernetes-client-1.6.1-3.3 \
-    kubernetes-kubelet-1.6.1-3.3 \
-    kubernetes-master-1.6.1-3.3 \
-    kubernetes-node-1.6.1-3.3 \
-    kubernetes-addons-kubedns-1.5.3-1.1 \
-    kubernetes-node-cni-1.5.3-1.1 \
-    kubernetes-node-image-pause-0.1-1.3
+    docker \
+    etcd-3.1.0 \
+    kubernetes-common-1.6.1 \
+    kubernetes-client-1.6.1 \
+    kubernetes-kubelet-1.6.1 \
+    kubernetes-master-1.6.1 \
+    kubernetes-node-1.6.1 \
+    kubernetes-addons-kubedns-1.5.3 \
+    kubernetes-node-cni-1.5.3 \
+    kubernetes-node-image-pause-0.1
 
+usermod --append --groups docker vagrant
 systemctl enable etcd.service
 systemctl enable kube-apiserver.service
 systemctl enable kube-controller-manager.service
