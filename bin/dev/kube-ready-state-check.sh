d0bf4f877dbde8e4befcf84464ad4c6a4ab711b2
Andreas Kupries <andreas.kupries@suse.com>
2017-07-28 11:00:55 -0700

    Added test for tiller to the readiness checker. Tweaked the messages a bit.

--- bin/dev/kube-ready-state-check.sh
+++ bin/dev/kube-ready-state-check.sh
@@ -87,7 +87,13 @@ fi
 # kube-dns shows 4/4 ready
 if having_category kube ; then
     kubectl get pods --namespace=kube-system --selector k8s-app=kube-dns | grep -Eq '([0-9])/\1 *Running'
-    status "kube-dns should shows 4/4 ready"
+    status "kube-dns should be running (show 4/4 ready)"
+fi
+
+# tiller-deploy shows 4/4 ready
+if having_category kube ; then
+    kubectl get pods --namespace=kube-system --selector name=tiller | grep -Eq '([0-9])/\1 *Running'
+    status "tiller should be running (1/1 ready)"
 fi
 
 # ntp is installed and running
