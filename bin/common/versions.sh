ce3b49a22f028cef0aed60960cf128cb8acd1671
Andreas Kupries <andreas.kupries@suse.com>
2017-11-13 12:50:18 -0800

    Fissile bump.

--- bin/common/versions.sh
+++ bin/common/versions.sh
@@ -9,7 +9,7 @@ set -o errexit -o nounset
 
 export BOSH_CLI_VERSION="39747e9d1fbc1d32af3672f903b6c4b73e1e1a9e"
 export CFCLI_VERSION="6.21.1"
-export FISSILE_VERSION="5.0.0+299.ga9b2d92"
+export FISSILE_VERSION="5.0.0+312.gafc2831"
 export HELM_VERSION="2.6.2"
 export HELM_CERTGEN_VERSION="master"
 export CERTSTRAP_VERSION="v1.0.1-11-g0e00d5c"
