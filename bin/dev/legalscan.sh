892841810fe8246f6273022ca104264fe1ed599c
Jonathan Brielmaier <jbrielmaier@suse.de>
2017-11-13 13:44:56 +0100

    Create separate submit request for each package

--- bin/dev/legalscan.sh
+++ bin/dev/legalscan.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 PROJECTS="Cloud:Platform:sources:scf Cloud:Platform:sources:buildpacks"
-TMPPATH=TMPOSC
-rm -r "${TMPPATH}"
-mkdir -p "${TMPPATH}" 
-pushd "${TMPPATH}" > /dev/null
 for PROJECT in ${PROJECTS}; do
-  osc checkout -M "${PROJECT}"
-  pushd "${PROJECT}" > /dev/null
-  osc submitrequest ${PROJECT}:reviewed
-  popd > /dev/null
+  for PACKAGE in $(osc ls $PROJECT); do
+    osc submitrequest --yes $PROJECT $PACKAGE ${PROJECT}:reviewed
+  done
 done
