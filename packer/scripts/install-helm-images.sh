c47dbe0b559bd1afbcda5d2eaf6e9c656fab6a56
Aaron Lefkowitz <aaron.lefkowitz@suse.com>
2017-12-11 16:12:26 -0800

    Bump SCF and Console in cached VM

--- packer/scripts/install-helm-images.sh
+++ packer/scripts/install-helm-images.sh
@@ -3,7 +3,7 @@
 set -o errexit
 set -o xtrace
 
-wget -O /tmp/txtplate https://github.com/aarondl/txtplate/releases/download/v0.0.3/txtplate-linux-amd64
+wget -O /tmp/txtplate https://github.com/aarondl/txtplate/releases/download/v0.0.4/txtplate-linux-amd64
 chmod +x /tmp/txtplate
 
 get_helm_images() {
@@ -14,7 +14,7 @@ get_helm_images() {
         DIR=$(dirname "${i}")
         grep --fixed-string --no-filename 'image:' ${DIR}/templates/* \
             | sed 's@\.Values@@g' \
-            | /tmp/txtplate "${VALUES:-${DIR}/values.yaml}" \
+            | /tmp/txtplate "${DIR}/values.yaml" "${VALUES}" \
             | grep --extended --only-matching '([^"/[:space:]]+/)?[^"/[:space:]]+/[^:[:space:]]+:[a-zA-Z0-9\._-]+' \
             | xargs --no-run-if-empty -n1 docker pull
     done
@@ -30,5 +30,5 @@ cat > /tmp/values.json <<EOF
 EOF
 
 get_helm_images /home/scf/helm /tmp/values.json
-get_helm_images /home/scf/console
-get_helm_images /home/scf/console/charts/mariadb
+get_helm_images /home/scf/console /tmp/values.json
+get_helm_images /home/scf/console/charts/mariadb /tmp/values.json
