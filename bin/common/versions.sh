082781b2eff0c17949d2efe000dee0c74000e588
Andreas Kupries <andreas.kupries@suse.com>
2017-12-14 12:08:34 -0800

    Bump stemcell.

--- bin/common/versions.sh
+++ bin/common/versions.sh
@@ -22,7 +22,7 @@ export STAMPY_MAJOR=$(echo "$STAMPY_VERSION" | sed -e 's/\.g.*//' -e 's/\.[^.]*$
 
 # Used in: .envrc
 
-export FISSILE_STEMCELL_VERSION=${FISSILE_STEMCELL_VERSION:-42.2-15.g38c573e-29.48}
+export FISSILE_STEMCELL_VERSION=${FISSILE_STEMCELL_VERSION:-42.2-21.g0757523-29.55}
 
 # Used in: bin/generate-dev-certs.sh
 
