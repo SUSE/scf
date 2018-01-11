a8fcf6b32d5b5400c32db22906c7c11289655f66
Mark Yen <mark.yen@suse.com>
2018-01-11 12:32:12 -0800

    forward_logfiles.sh: cleanup
    This shouldn't change what the code is doing yet, just shellcheck
    related fixes.

--- container-host-files/etc/scf/config/scripts/forward_logfiles.sh
+++ container-host-files/etc/scf/config/scripts/forward_logfiles.sh
@@ -1,60 +1,61 @@
-#used for initial config
+#!/usr/bin/env bash
+
+# This script sets up rsyslog to forward logs in /var/vcap/sys/log to a remote
+# host
+
+# used for initial config
 MAIN_CONFIG="90-vcap.conf"
 RSYSLOG_CONF_DIR="/etc/rsyslog.d"
 
-#used for adding individual logs
-BACKUP_WATCH_DIR=/var/vcap/sys/log #in case no ENV variable is set for RSYSLOG_FORWARDER_WATCH_DIR
-RSYSLOG_CONF_PREFIX=91-vcap
-RSYSLOG_CONF_DIR=/etc/rsyslog.d
+# used for adding individual logs
+BACKUP_WATCH_DIR="/var/vcap/sys/log" #in case no ENV variable is set for RSYSLOG_FORWARDER_WATCH_DIR
+RSYSLOG_CONF_PREFIX="91-vcap"
 IGNORE_DIR="gocode"
 TARGET_NAME=
 TARGET_BASENAME=
 
-SCRIPT_FILE=/usr/sbin/forward_logfiles.sh
-PB_OUT=/var/log/pb.out
+SCRIPT_FILE="/usr/sbin/forward_logfiles.sh"
+PB_OUT="/var/log/pb.out"
 
-if [ ! -f $SCRIPT_FILE ]; then
-	echo "#log forwarding script" > $PB_OUT
+if [ ! -f "${SCRIPT_FILE}" ]; then
+	echo "#log forwarding script" > "${PB_OUT}"
 fi
 
 if [ -z "${RSYSLOG_FORWARDER_WATCH_DIR:-}" ]; then
-        RSYSLOG_FORWARDER_WATCH_DIR=$BACKUP_WATCH_DIR
-        echo "RSYSLOG_FORWARDER_WATCH_DIR not set. Using default $BACKUP_WATCH_DIR" >> $PB_OUT
+        RSYSLOG_FORWARDER_WATCH_DIR="${BACKUP_WATCH_DIR}"
+        echo "RSYSLOG_FORWARDER_WATCH_DIR not set. Using default ${BACKUP_WATCH_DIR}" >> "${PB_OUT}"
 fi
 
-if [ ! -d "$RSYSLOG_FORWARDER_WATCH_DIR" ]; then
-        echo "$RSYSLOG_FORWARDER_WATCH_DIR is not a valid directory" >> $PB_OUT
+if [ ! -d "${RSYSLOG_FORWARDER_WATCH_DIR}" ]; then
+        echo "${RSYSLOG_FORWARDER_WATCH_DIR} is not a valid directory" >> "${PB_OUT}"
 fi
 
-if [ -z "$SCF_LOG_HOST" ]; then
-    echo "SCF_LOG_HOST is not set" >> $PB_OUT
+if [ -z "${SCF_LOG_HOST}" ]; then
+    echo "SCF_LOG_HOST is not set" >> "${PB_OUT}"
     exit 0
 fi
-
-_SCF_LOG_CONFIG=$SCF_LOG_HOST:$SCF_LOG_PORT
 
 function appendToCron {
 	echo appending to cron
         crontab -l > tempcrontab
-        echo "export _SCF_LOG_CONFIG=$SCF_LOG_CONFIG" >> $SCRIPT_FILE
-        echo "export RSYSLOG_FORWARDER_WATCH_DIR=$RSYSLOG_FORWARDER_WATCH_DIR" >> $SCRIPT_FILE
-        echo "export SCF_LOG_HOST=$SCF_LOG_HOST" >> $SCRIPT_FILE
-        echo "export SCF_LOG_PORT=$SCF_LOG_PORT" >> $SCRIPT_FILE
-        echo "export SCF_LOG_PREFIX=$SCF_LOG_PREFIX" >> $SCRIPT_FILE
-        echo "export SCF_LOG_PROTOCOL=$SCF_LOG_PROTOCOL" >> $SCRIPT_FILE
-        cat $0 >> $SCRIPT_FILE
-        echo "*/1 * * * * bash $SCRIPT_FILE >> /dev/null 2>&1" >> tempcrontab
+        echo "export RSYSLOG_FORWARDER_WATCH_DIR=${RSYSLOG_FORWARDER_WATCH_DIR}" >> "${SCRIPT_FILE}"
+        echo "export SCF_LOG_HOST=${SCF_LOG_HOST}" >> "${SCRIPT_FILE}"
+        echo "export SCF_LOG_PORT=${SCF_LOG_PORT}" >> "${SCRIPT_FILE}"
+        echo "export SCF_LOG_PREFIX=${SCF_LOG_PREFIX}" >> "${SCRIPT_FILE}"
+        echo "export SCF_LOG_PROTOCOL=${SCF_LOG_PROTOCOL}" >> "${SCRIPT_FILE}"
+        cat "$0" >> "${SCRIPT_FILE}"
+        echo "*/1 * * * * /usr/bin/env bash ${SCRIPT_FILE} >> /dev/null 2>&1" >> tempcrontab
         crontab tempcrontab
 }
 
-#check if cron has something in it
+# check if cron has something in it
 if crontab -l &>/dev/null ; then
-        #put the script in cron if it is not there already
+        # Put the script in cron if it is not there already
         if ! { crontab -l | grep forward_logfiles.sh ; } ; then
                 appendToCron
         fi
 else
-        echo "#creating cron conf as it does not exist yet" >> $PB_OUT
+        echo "# Creating cron conf as it does not exist yet" >> "${PB_OUT}"
         appendToCron
 fi
 
@@ -63,7 +64,7 @@ fi
 # destination
 function initialConfig {
 
-	case ${SCF_LOG_PROTOCOL} in
+	case "${SCF_LOG_PROTOCOL}" in
 	    udp)
 		SCF_LOG_PREFIX=
 		;;
@@ -71,7 +72,7 @@ function initialConfig {
 		SCF_LOG_PREFIX=@
 		;;
 	    *)
-                echo "Rsyslog forwarder: Bad protocol ${...}, could not create $MAIN_CONFIG in $RSYSLOG_CONF_DIR" >> $PB_OUT
+                echo "Rsyslog forwarder: Bad protocol ${SCF_LOG_PROTOCOL}, could not create ${MAIN_CONFIG} in ${RSYSLOG_CONF_DIR}" >> "${PB_OUT}"
                 exit 0
 		;;
 	esac
@@ -85,12 +86,12 @@ function initialConfig {
                 *.* @${SCF_LOG_PREFIX}${SCF_LOG_HOST}:${SCF_LOG_PORT}
                 :app-name, contains, "vcap" ${HOME}
 	EOF
-                echo "Rsyslog forwarder: Could not create $MAIN_CONFIG in $RSYSLOG_CONF_DIR" >> $PB_OUT
+                echo "Rsyslog forwarder: Could not create ${MAIN_CONFIG} in ${RSYSLOG_CONF_DIR}" >> "${PB_OUT}"
                 exit 0
         fi
 
-        if [[ ! -f "$RSYSLOG_CONF_DIR/$MAIN_CONFIG" ]]; then
-                echo "Rsyslog forwarder: File $MAIN_CONFIG not found in $RSYSLOG_CONF_DIR" >> $PB_OUT
+        if [[ ! -f "${RSYSLOG_CONF_DIR}/${MAIN_CONFIG}" ]]; then
+                echo "Rsyslog forwarder: File ${MAIN_CONFIG} not found in ${RSYSLOG_CONF_DIR}" >> "${PB_OUT}"
                 exit 0
         fi
 }
@@ -98,36 +99,36 @@ function initialConfig {
 # check if more logs to be monitored by rsyslog have come into existence since the last run
 function searchTargetDir {
         filesAdded=1
-        for file in $1/*
+        for file in "$1"/*
         do
-                if [ -d $file ]; then
+                if [ -d "${file}" ]; then
                         ignored=false
-                        for ignore in $IGNORE_DIR
+                        for ignore in ${IGNORE_DIR}
                         do
-                                if [[ $file == $RSYSLOG_FORWARDER_WATCH_DIR/$ignore ]]; then
-                                        echo "Ignoring $file directory"
+                                if [[ "${file}" == "${RSYSLOG_FORWARDER_WATCH_DIR}/${ignore}" ]]; then
+                                        echo "Ignoring ${file} directory"
                                         ignored=true
                                 fi
                         done;
-                        if [ $ignored == false ]; then
-                                if searchTargetDir $file; then
+                        if [ "${ignored}" == false ]; then
+                                if searchTargetDir "${file}"; then
                                         filesAdded=0
                                 fi
                         fi
                 else
                         if [ "${file: -4}" == ".log" ]; then
-                                targetName $file
-                                if checkConfigExists $file; then
-                                        echo $TARGET_NAME exists
+                                targetName "${file}"
+                                if checkConfigExists "${file}"; then
+                                        echo "${TARGET_NAME} exists"
                                 else
-                                        echo "Creating $TARGET_NAME"
-                                        createTargetConf $file
+                                        echo "Creating ${TARGET_NAME}"
+                                        createTargetConf "${file}"
                                         filesAdded=0
                                 fi
                         fi
                 fi
         done
-        return $filesAdded
+        return "${filesAdded}"
 }
 
 #Create the rsyslog configuration file inside rsysconf.d
@@ -144,13 +145,13 @@ function createTargetConf {
 }
 
 function targetName {
-        filename=$(basename $1)
+        filename="$(basename "$1")"
         TARGET_BASENAME="${filename%.*}"
 }
 
 function checkConfigExists {
-        TARGET_NAME=$RSYSLOG_CONF_DIR/$RSYSLOG_CONF_PREFIX-$TARGET_BASENAME.conf
-        if [ -f $TARGET_NAME ]; then
+        TARGET_NAME="${RSYSLOG_CONF_DIR}/${RSYSLOG_CONF_PREFIX}-${TARGET_BASENAME}.conf"
+        if [ -f "${TARGET_NAME}" ]; then
                 return 0
         else
                 return 1
@@ -158,7 +159,7 @@ function checkConfigExists {
 }
 
 #check if the forwarding conf is set up
-if [ ! -f $RSYSLOG_CONF_DIR/$MAIN_CONFIG ]; then
+if [ ! -f "${RSYSLOG_CONF_DIR}/${MAIN_CONFIG}" ]; then
       echo creating initial config for forwarding
       initialConfig
 else
@@ -166,9 +167,9 @@ else
 fi
 
 #make sure that configurations (per log-file) are added to the rsyslog.d folder
-if searchTargetDir $RSYSLOG_FORWARDER_WATCH_DIR; then
+if searchTargetDir "${RSYSLOG_FORWARDER_WATCH_DIR}"; then
         if test -r /var/run/rsyslog.pid; then
-                if test -d /proc/$(cat /var/run/rsyslog.pid); then
+                if test -d "/proc/$(cat /var/run/rsyslog.pid)"; then
                         service rsyslog restart
                 fi
         fi
