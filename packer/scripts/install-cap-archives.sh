be42cadd7f2a2c4b61902328e5642dff8278cb26
Alex Harford <alex.harford@suse.com>
2018-02-23 14:54:40 -0800

    Update cached Vagrant box
    * SCF v2.7.0
    * Console v1.0.0

--- packer/scripts/install-cap-archives.sh
+++ packer/scripts/install-cap-archives.sh
@@ -4,13 +4,13 @@ set -o errexit
 set -o verbose
 
 # Fetch predetermined version of scf
-SCF_URL=https://github.com/SUSE/scf/releases/download/2.4.1-beta3/scf-opensuse-2.4.1-beta3.cf278.0.g12f5d3a8.linux-amd64.zip
+SCF_URL=https://github.com/SUSE/scf/releases/download/2.7.0/scf-opensuse-2.7.0.cf1.9.0.0.g2d95fcb5.linux-amd64.zip
 SCF_ARCHIVE="$(basename "${SCF_URL}")"
 wget "${SCF_URL}"
 unzip "${SCF_ARCHIVE}"
 
 # Fetch predetermined version of the ui
-CONSOLE_URL=https://github.com/SUSE/stratos-ui/releases/download/0.9.9/console-helm-chart-0.9.9.tgz
+CONSOLE_URL=https://github.com/cloudfoundry-incubator/stratos/releases/download/1.0.0/console-helm-chart-1.0.0.tgz
 CONSOLE_ARCHIVE="$(basename "${CONSOLE_URL}")"
 wget "${CONSOLE_URL}"
 tar xzf "${CONSOLE_ARCHIVE}"
