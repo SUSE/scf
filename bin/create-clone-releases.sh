b5cb4b0f88c1636f8c2b178d9997ecc0452355ac
Mark Yen <mark.yen@suse.com>
2018-03-23 16:04:51 -0700

    Update CF bumping scripts
    - No more hard coded things please
    - Instead of looking for things named `-clone`, look at the fissile list
    - Avoid piping to tee (breaks errexit)

--- bin/create-clone-releases.sh
+++ bin/create-clone-releases.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env bash
-set -e
+set -o errexit
 
 GIT_ROOT=${GIT_ROOT:-$(git rev-parse --show-toplevel)}
 
 mkdir -p ${GIT_ROOT}/_work/LOG/ccr
-for clone in $(find . -type d -name '*-clone')
-do
-    release=$(echo $(basename $clone) | sed -e 's/-clone//' -e 's/-release//')
-
+for dir in ${FISSILE_RELEASE//,/ } ; do
+    clone="${dir#${PWD}/}-clone"
+    if ! test -d "${clone}" ; then
+        continue
+    fi
+    release="$(basename "${dir}" -release)"
     echo
-    echo ___ ___ ___ ___ ___ ___ ___ ___ ___ $release @ $clone ___
+    echo ___ ___ ___ ___ ___ ___ ___ ___ ___ "${release}" @ "${clone#${GIT_ROOT}/}" ___
     echo
-    (
-	case $release in
-	    *cf-mysql*)
-		RUBY_VERSION=2.3.1 ${GIT_ROOT}/bin/create-release.sh $clone $release
-		;;
-	    *)
-		${GIT_ROOT}/bin/create-release.sh $clone $release
-		;;
-	esac
-    ) 2>&1 | tee ${GIT_ROOT}/_work/LOG/ccr/${release}
+    unset RUBY_VERSION
+    case "${release}" in
+        *cf-mysql*) export RUBY_VERSION=2.3.1 ;;
+    esac
+    "${GIT_ROOT}/bin/create-release.sh" "${clone}" "${release}" \
+        > >( tee "${GIT_ROOT}/_work/LOG/ccr/${release}" ) 2>&1
 done
