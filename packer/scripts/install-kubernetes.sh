6340dd3e5c5f67d8503fc52244dfc81e9e0ae940
Mark Yen <mark.yen@suse.com>
2018-04-13 18:08:12 -0700

    packer: Upgrade kubernetes, fixes for that.
    The old repo (home:aarondl) no longer seems to exist...

--- packer/scripts/install-kubernetes.sh
+++ packer/scripts/install-kubernetes.sh
@@ -7,29 +7,21 @@ set -o errexit -o xtrace
 # Kube doesn't like swap: https://github.com/kubernetes/kubernetes/blob/e4551d50e57c089aab6f67333412d3ca64bc09ae/pkg/kubelet/cm/container_manager_linux.go#L207-L209
 swapoff -a
 
-zypper --non-interactive addrepo --gpgcheck --refresh --priority 130 --check \
-    obs://devel:CaaSP:Head:ControllerNode devel:CaaSP
 zypper --non-interactive addrepo --gpgcheck --refresh --priority 120 --check \
-    obs://home:aarondl:branches:devel:CaaSP:Head:ControllerNode aaron:CaaSP
-zypper --non-interactive addrepo --gpgcheck --refresh --priority 110 --check \
-    obs://Virtualization:containers Virtualization:containers
+    obs://home:mook_work:branches:devel:CaaSP:Head:ControllerNode mook_work:CaaSP
 
 zypper --non-interactive --gpg-auto-import-keys refresh
-zypper --non-interactive repos --uri # for troubleshooting
+zypper --non-interactive repos --uri --priority # for troubleshooting
 
-zypper --non-interactive install --no-confirm --from=Virtualization:containers \
-    'docker = 17.09.1_ce'
-
-zypper --non-interactive install --no-confirm --from=devel:CaaSP \
+zypper --non-interactive install --no-confirm --from=mook_work:CaaSP \
     etcd \
     cni-plugins \
-    kubernetes-node-image-pause
-
-zypper --non-interactive install --no-confirm --from=aaron:CaaSP \
     kubernetes-client \
     kubernetes-kubelet \
     kubernetes-master \
-    kubernetes-node
+    kubernetes-node \
+    kubernetes-node-image-pause
+# kubernetes-kubelet pulls in docker-kubic automatically
 
 usermod --append --groups docker vagrant || usermod --append --groups docker scf
 systemctl enable etcd.service
