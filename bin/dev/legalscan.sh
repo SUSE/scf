11ab2231241923c4fde5cda7d902a82cc300d446
Tim Hardeck <thardeck@suse.de>
2018-04-19 10:26:01 +0200

    Add sidecars to legalscan.sh
    Signed-off-by: Christian Richter <crichter@suse.com>

--- bin/dev/legalscan.sh
+++ bin/dev/legalscan.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-PROJECTS="Cloud:Platform:sources:scf Cloud:Platform:sources:buildpacks"
+PROJECTS="Cloud:Platform:sources:scf Cloud:Platform:sources:buildpacks Cloud:Platform:sources:sidecars"
 for PROJECT in ${PROJECTS}; do
   for PACKAGE in $(osc ls $PROJECT); do
     osc submitrequest --yes --message=legal-review $PROJECT $PACKAGE ${PROJECT}:reviewed
