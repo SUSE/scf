d015127ec472d3639394081e95deb6b1a3aafe59
Alex Harford <alex.harford@suse.com>
2018-06-07 09:55:18 -0700

    Point fissile to master
    The active/passive role branch has been merged now.

--- bin/common/versions.sh
+++ bin/common/versions.sh
@@ -9,7 +9,7 @@ set -o errexit -o nounset
 
 export BOSH_CLI_VERSION="fcaa9c6caff58ab8da8c56481320681cdea492ee"
 export CFCLI_VERSION="6.21.1"
-export FISSILE_VERSION="5.2.0+121.g14e99d1"
+export FISSILE_VERSION="5.2.0+128.g0b433b9"
 export HELM_VERSION="2.6.2"
 export KK_VERSION="576a42386770423ced46ab4ae9955bee59b0d4dd"
 export KUBECTL_VERSION="1.8.2"
