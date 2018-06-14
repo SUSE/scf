6b3ba459caaeba8a5f0dee2093656d9fc2a93bb7
Mark Yen <mark.yen@suse.com>
2018-06-14 13:09:33 -0700

    make/tests: Allow manual environment variable overrides
    e.g. make/tests acceptance-tests env.CATS_SUITES="=apps"

--- bin/kube_overrides.rb
+++ bin/kube_overrides.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 require 'json'
 
-namespace, domain, kube_config = ARGV
+namespace, domain, kube_config = ARGV.shift(3)
 
 configmap = %x(kubectl get configmap secrets-config --namespace #{namespace} -o json)
 configmap = JSON.parse(configmap)['data']
@@ -12,6 +12,12 @@ secrets = JSON.parse(secrets)['data']
 
 generated = %x(kubectl get secret #{current_secrets_name} --namespace #{namespace} -o json)
 generated = JSON.parse(generated)['data']
+
+overrides = Hash.new
+ARGV.each do |arg|
+  k, v = arg.split('=', 2)
+  overrides[k.split('.')] = v
+end
 
 obj = YAML.load_file(kube_config)
 obj['spec']['containers'].each do |container|
@@ -35,6 +41,28 @@ obj['spec']['containers'].each do |container|
       name = env['name'].downcase.gsub('_', '-')
       if generated.has_key?(name) && (secrets[name].nil? || secrets[name].empty?)
         env['valueFrom']['secretKeyRef']['name'] = current_secrets_name
+      end
+    end
+    overrides.each do |k, v|
+      child = container
+      k[0...-1].each do |elem|
+        child[elem] ||= {}
+        child = child[elem]
+      end
+      case child
+      when Array
+        # Deal with the environment list specially
+        child.reject! do |elem|
+          elem['name'] == k.last
+        end
+        child << {
+          'name'  => k.last,
+          'value' => v,
+        }
+      when Hash
+        child[k.last] = v
+      else
+        raise ArgumentError, "Don't know how to deal with a #{child.class} from #{k}"
       end
     end
   end
