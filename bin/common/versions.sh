445f775fb27e1ba37413275be108be390fa1034f
Alex Harford <alex.harford@suse.com>
2018-08-08 14:40:32 -0700

    Rename role-manifest keys to match fissile 6+
    Top level `roles` -> `instance_groups`
    
    Job's `release_name` -> `release`

--- bin/common/versions.sh
+++ bin/common/versions.sh
@@ -9,7 +9,7 @@ set -o errexit -o nounset
 
 export BOSH_CLI_VERSION="fcaa9c6caff58ab8da8c56481320681cdea492ee"
 export CFCLI_VERSION="6.37.0"
-export FISSILE_VERSION="5.3.0+16.gec5724d"
+export FISSILE_VERSION="6.0.0+0.g711088f"
 export HELM_VERSION="2.9.1"
 export KK_VERSION="576a42386770423ced46ab4ae9955bee59b0d4dd"
 export KUBECTL_VERSION="1.9.6"
