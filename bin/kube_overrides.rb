14a10869a2c7e584dccdf4de3347cd415fdb859d
Andreas Kupries <andreas.kupries@suse.com>
2018-09-20 12:24:06 -0700

    [#PMrNrMOh] Bump SCF to fissile de-cluttering the helm chart.
    Updated `kube_overrides.rb` to handle the multi-doc yaml generated by
    that fissile for role `acceptance-tests-brain`.

--- bin/kube_overrides.rb
+++ bin/kube_overrides.rb
@@ -19,54 +19,57 @@ ARGV.each do |arg|
   overrides[k.split('.')] = v
 end
 
-obj = YAML.load_file(kube_config)
-obj['spec']['containers'].each do |container|
-  container['env'].each do |env|
-    unless domain.empty?
-      value = env['value']
+YAML.load_stream (IO.read(kube_config)) do |obj|
+  if obj['spec'] then
+    obj['spec']['containers'].each do |container|
+      container['env'].each do |env|
+        unless domain.empty?
+          value = env['value']
 
-      case env['name']
-      when 'DOMAIN'
-        value = domain
-      when 'TCP_DOMAIN'
-        value = "tcp.#{domain}"
-      when 'UAA_HOST'
-        value = "uaa.#{domain}"
+          case env['name']
+          when 'DOMAIN'
+            value = domain
+          when 'TCP_DOMAIN'
+            value = "tcp.#{domain}"
+          when 'UAA_HOST'
+            value = "uaa.#{domain}"
+          end
+
+          env['value'] = value.to_s
+        end
+
+        if env['valueFrom'] && env['valueFrom']['secretKeyRef']
+          name = env['name'].downcase.gsub('_', '-')
+          if generated.has_key?(name) && (secrets[name].nil? || secrets[name].empty?)
+            env['valueFrom']['secretKeyRef']['name'] = current_secrets_name
+          end
+        end
       end
 
-      env['value'] = value.to_s
-    end
-
-    if env['valueFrom'] && env['valueFrom']['secretKeyRef']
-      name = env['name'].downcase.gsub('_', '-')
-      if generated.has_key?(name) && (secrets[name].nil? || secrets[name].empty?)
-        env['valueFrom']['secretKeyRef']['name'] = current_secrets_name
+      overrides.each do |k, v|
+        child = container
+        k[0...-1].each do |elem|
+          child[elem] ||= {}
+          child = child[elem]
+        end
+        if k[0...1] == %w(env)
+          # Deal with the environment list specially, because the syntax isn't what
+          # humans normally want.
+          # The environment is actually in a list of hashes with "name" and "value"
+          # keys.  Erase any elements with the same name, and then append it.
+          child.reject! do |elem|
+            elem['name'] == k.last
+          end
+          child << {
+            'name'  => k.last,
+            'value' => v,
+          }
+        else
+          # Normal key/value override, e.g. to change the image pull policy
+          child[k.last] = v
+        end
       end
     end
   end
-
-  overrides.each do |k, v|
-    child = container
-    k[0...-1].each do |elem|
-      child[elem] ||= {}
-      child = child[elem]
-    end
-    if k[0...1] == %w(env)
-      # Deal with the environment list specially, because the syntax isn't what
-      # humans normally want.
-      # The environment is actually in a list of hashes with "name" and "value"
-      # keys.  Erase any elements with the same name, and then append it.
-      child.reject! do |elem|
-        elem['name'] == k.last
-      end
-      child << {
-        'name'  => k.last,
-        'value' => v,
-      }
-    else
-      # Normal key/value override, e.g. to change the image pull policy
-      child[k.last] = v
-    end
-  end
+  puts obj.to_yaml
 end
-puts obj.to_json
