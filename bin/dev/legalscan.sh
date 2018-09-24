0303f73b240600952f63867be308470afdc41de9
Tim Hardeck <thardeck@suse.de>
2018-09-24 14:07:58 +0200

    Submit OBS built buildpack dependencies for legal review

--- bin/dev/legalscan.sh
+++ bin/dev/legalscan.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+for PACKAGE in $(osc ls Cloud:Platform:buildpacks:dependencies); do
+  osc submitrequest --yes --message=legal-review Cloud:Platform:buildpacks:dependencies $PACKAGE Cloud:Platform:sources:buildpacks:reviewed
+done
+
 PROJECTS="Cloud:Platform:sources:scf Cloud:Platform:sources:buildpacks Cloud:Platform:sources:sidecars"
 for PROJECT in ${PROJECTS}; do
   for PACKAGE in $(osc ls $PROJECT); do
