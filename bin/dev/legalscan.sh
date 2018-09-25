6b1be10b03831fff07104f89fd0a6d0ad88b2a76
Tim Hardeck <thardeck@suse.de>
2018-09-25 14:04:25 +0200

    Use the same project structure as for sources

--- bin/dev/legalscan.sh
+++ bin/dev/legalscan.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-for PACKAGE in $(osc ls Cloud:Platform:buildpacks:dependencies); do
-  osc submitrequest --yes --message=legal-review Cloud:Platform:buildpacks:dependencies $PACKAGE Cloud:Platform:sources:buildpacks:reviewed
-done
-
-PROJECTS="Cloud:Platform:sources:scf Cloud:Platform:sources:buildpacks Cloud:Platform:sources:sidecars"
+PROJECTS="Cloud:Platform:sources:scf Cloud:Platform:sources:buildpacks Cloud:Platform:buildpacks:dependencies Cloud:Platform:sources:sidecars"
 for PROJECT in ${PROJECTS}; do
   for PACKAGE in $(osc ls $PROJECT); do
     osc submitrequest --yes --message=legal-review $PROJECT $PACKAGE ${PROJECT}:reviewed
