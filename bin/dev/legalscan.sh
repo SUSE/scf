5455a02b33b5de205220bd1877a0ba4f9bec5097
Tim Hardeck <thardeck@suse.de>
2018-09-25 14:05:53 +0200

    Use more explicit variable names in legalscan

--- bin/dev/legalscan.sh
+++ bin/dev/legalscan.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 PROJECTS="Cloud:Platform:sources:scf Cloud:Platform:sources:buildpacks Cloud:Platform:buildpacks:dependencies Cloud:Platform:sources:sidecars"
-for PROJECT in ${PROJECTS}; do
-  for PACKAGE in $(osc ls $PROJECT); do
-    osc submitrequest --yes --message=legal-review $PROJECT $PACKAGE ${PROJECT}:reviewed
+for SOURCE_OBS_PROJECT in ${PROJECTS}; do
+  for PACKAGE in $(osc ls ${SOURCE_OBS_PROJECT}); do
+    TARGET_OBS_PROJECT="${SOURCE_OBS_PROJECT}:reviewed"
+    osc submitrequest --yes --message=legal-review ${SOURCE_OBS_PROJECT} ${PACKAGE} ${TARGET_OBS_PROJECT}
   done
 done
