4f51d4b564c17a48c444bff542357b4befd0f143
viovanov <vlad.iovanov@suse.com>
2018-10-03 14:31:41 +0300

    Remove diego pieces that are no longer required

--- container-host-files/etc/scf/config/scripts/cleanup-garden-graph.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -o errexit -o nounset -o xtrace
-
-rm -rf /var/vcap/data/grootfs/*
-
-# Ensure that runc and container processes can stat everything
-chmod ugo+rx /var/vcap/data/grootfs
