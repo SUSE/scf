917589c8c88d72da13b15d9b37cb691b7affd8e6
Andreas Kupries <andreas.kupries@suse.com>
2018-11-05 16:07:58 -0800

    Address easy comment, quoting.

--- container-host-files/etc/scf/config/scripts/configure-az.sh
+++ container-host-files/etc/scf/config/scripts/configure-az.sh
@@ -2,8 +2,8 @@
 
 if test -z "${AZ_LABEL_NAME}"
 then
-    echo AZ: No label configured
-    echo AZ: Skipping
+    echo "AZ: No label configured"
+    echo "AZ: Skipping"
 else
     # AZ override processing is in effect.
 
@@ -23,8 +23,8 @@ else
 
     node="$($kubectl get pod $(hostname) -o jsonpath='{.spec.nodeName}')"
 
-    echo AZ: Configured ${AZ_LABEL_NAME}
-    echo AZ: Node...... ${node}
+    echo "AZ: Configured ${AZ_LABEL_NAME}"
+    echo "AZ: Node...... ${node}"
 
     # Determine the AZ of the kube worker node and make this
     # information available to the container, scripts, and binaries of
@@ -35,12 +35,12 @@ else
 
     if test -z "${NODE_AZ}"
     then
-	echo AZ: No information found
-	echo AZ: Skipping
+	echo "AZ: No information found"
+	echo "AZ: Skipping"
     else
 	# Propagate the found AZ information into cloudfoundry
 
-	echo AZ: Found..... ${NODE_AZ}
+	echo "AZ: Found..... ${NODE_AZ}"
         export KUBE_AZ="${NODE_AZ}"
     fi
 fi
