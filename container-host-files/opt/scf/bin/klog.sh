ac2776eb563e1336543fe99186a995bfb82d81ee
Bikramdeep Singh <bikramdeep.singh@suse.com>
2019-01-07 14:32:31 -0800

    Changes for the code review

--- container-host-files/opt/scf/bin/klog.sh
+++ container-host-files/opt/scf/bin/klog.sh
@@ -33,66 +33,45 @@ function get_phase() {
 }
 
 function check_for_log_dir() {
-  kubectl exec "${POD}" --namespace "${NS}" --container "${CONTAINER}" -- bash -c "[ -d /var/vcap/sys/log/"${CONTAINER}" ]" 2> /dev/null
+  kubectl exec "${POD}" --namespace "${NS}" --container "${CONTAINER}" -- bash -c "[ -d /var/vcap/sys/log ]" 2> /dev/null
 }
 
 if [ ! -f "${DONE}" ]; then
   rm -rf "${KLOG:?}/${NS:?}"
-  BASE_DIR="${KLOG}/${NS}"
+  NAMESPACE_DIR="${KLOG}/${NS}"
 
-  # Retrieving all the pods
+  # Retrieving all the pods.
   PODS=($(kubectl get pods --namespace "${NS}" --output=jsonpath='{.items[*].metadata.name}'))
 
   for POD in "${PODS[@]}"; do
-    POD_DIR="${BASE_DIR}/${POD}"
+    POD_DIR="${NAMESPACE_DIR}/${POD}"
 
-    # Retrieving all the containers within a pod
+    # Retrieving all the containers within a pod.
     CONTAINERS=($(kubectl get pods "${POD}" --namespace "${NS}" --output=jsonpath='{.spec.containers[*].name}'))
 
-    # Iterate over containers and dump logs
+    # Iterate over containers and dump logs.
     for CONTAINER in "${CONTAINERS[@]}"; do
 
-      CON_DIR="${POD_DIR}/${CONTAINER}"
-      mkdir -p ${CON_DIR}
+      CONTAINER_DIR="${POD_DIR}/${CONTAINER}"
+      mkdir -p ${CONTAINER_DIR}
 
-      # Get the CF logs inside the pod if there are any
+      # Get the CF logs inside the pod if there are any.
       if [ "$(get_phase)" != 'Succeeded' ] && check_for_log_dir; then
-        kubectl cp --namespace "${NS}" --container "${CONTAINER}" "${POD}":/var/vcap/sys/log/"${CONTAINER}"/ "${CON_DIR}/" 2> /dev/null
+        kubectl cp --namespace "${NS}" --container "${CONTAINER}" "${POD}":/var/vcap/sys/log/ "${CONTAINER_DIR}/" 2> /dev/null
       fi
 
+
       # Get the pod logs - previous may not be there if it was successful on the first run.
-      # Unfortunately we can't get anything past the previous one
-      kubectl logs "${POD}" --namespace "${NS}" --container "${CONTAINER}" > "${CON_DIR}/kube.log"
-      kubectl logs "${POD}" --namespace "${NS}" --container "${CONTAINER}" --previous > "${CON_DIR}/kube-previous.log" 2> /dev/null || true
-      kubectl describe pods "${POD}" --namespace "${NS}" > "${CON_DIR}/describe-pod.txt"
+      # Unfortunately we can't get anything past the previous one.
+      kubectl logs "${POD}" --namespace "${NS}" --container "${CONTAINER}" > "${CONTAINER_DIR}/kube.log"
+      kubectl logs "${POD}" --namespace "${NS}" --container "${CONTAINER}" --previous > "${CONTAINER_DIR}/kube-previous.log" 2> /dev/null || true
+      kubectl describe pods "${POD}" --namespace "${NS}" > "${CONTAINER_DIR}/describe-pod.txt"
     done
 
   done
-
-  # Unzip any logrotated files so the lookup can read them
-  gunzip -r "${KLOG}"
 
   kubectl get all --export=true --namespace "${NS}" --output=yaml > "${KLOG}/${NS}/resources.yaml"
   kubectl get events --export=true --namespace "${NS}" --output=yaml > "${KLOG}/${NS}/events.yaml"
 
   touch "${DONE}"
 fi
-
-NEWLINE=0
-function lookfor {
-  read PATTERN
-
-  cd "${KLOG}"
-  if grep -c -r -F "${PATTERN}" > .grep; then
-    [ "${NEWLINE}" == "1" ] && echo
-    NEWLINE=1
-
-    echo ">>> ${PATTERN}"
-    echo
-    grep -v :0$ .grep
-
-    while read INFO; do
-      echo "${INFO}"
-    done
-  fi
-}
