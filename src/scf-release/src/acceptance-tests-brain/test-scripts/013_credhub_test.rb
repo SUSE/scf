6d53369da73e547a95ca41c30bae304272601fd4
Thulio Ferraz Assis <f0rmiga@users.noreply.github.com>
2019-03-13 12:02:38 -0700

    WIP

--- src/scf-release/src/acceptance-tests-brain/test-scripts/013_credhub_test.rb
+++ src/scf-release/src/acceptance-tests-brain/test-scripts/013_credhub_test.rb
@@ -7,6 +7,8 @@ require 'json'
 NAMESPACE = ENV['KUBERNETES_NAMESPACE']
 STATEFULSET_NAME = 'credhub-user'
 
+Process.exit 1 if !statefulset_ready(NAMESPACE, STATEFULSET_NAME)
+
 # Check if credhub is running, otherwise skip the test.
 exit_skipping_test if !statefulset_ready(NAMESPACE, STATEFULSET_NAME)
 
