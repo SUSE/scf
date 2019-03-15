2579aad57d39ca822c9c5ee68e7fbfc580378fa6
Thulio Ferraz Assis <tassis@suse.com>
2019-03-15 09:21:24 -0700

    Set vcap limits for every role

--- container-host-files/etc/scf/config/scripts/set_vcap_limits.sh
+++ container-host-files/etc/scf/config/scripts/set_vcap_limits.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o nounset
 
-if [ -z "${DIEGO_VCAP_HARD_NPROC:-}" ] && [ -z "${DIEGO_VCAP_SOFT_NPROC:-}" ]; then
+if [ -z "${VCAP_HARD_NPROC:-}" ] && [ -z "${VCAP_SOFT_NPROC:-}" ]; then
   exit 0
 fi
 
@@ -12,23 +12,23 @@ print_err() {
   echo -e "\e[31m## ${1}" >&2
 }
 
-if [ -n "${DIEGO_VCAP_HARD_NPROC:-}" ] && [ -z "${DIEGO_VCAP_SOFT_NPROC:-}" ]; then
-  print_err "DIEGO_VCAP_SOFT_NPROC must be set when DIEGO_VCAP_HARD_NPROC is set"
+if [ -n "${VCAP_HARD_NPROC:-}" ] && [ -z "${VCAP_SOFT_NPROC:-}" ]; then
+  print_err "VCAP_SOFT_NPROC must be set when VCAP_HARD_NPROC is set"
   exit 1
 fi
 
-if [ -n "${DIEGO_VCAP_SOFT_NPROC:-}" ] && [ -z "${DIEGO_VCAP_HARD_NPROC:-}" ]; then
-  print_err "DIEGO_VCAP_HARD_NPROC must be set when DIEGO_VCAP_SOFT_NPROC is set"
+if [ -n "${VCAP_SOFT_NPROC:-}" ] && [ -z "${VCAP_HARD_NPROC:-}" ]; then
+  print_err "VCAP_HARD_NPROC must be set when VCAP_SOFT_NPROC is set"
   exit 1
 fi
 
-if (( "${DIEGO_VCAP_SOFT_NPROC}" > "${DIEGO_VCAP_HARD_NPROC}" )); then
-  print_err "DIEGO_VCAP_SOFT_NPROC (${DIEGO_VCAP_SOFT_NPROC}) cannot be larger than DIEGO_VCAP_HARD_NPROC (${DIEGO_VCAP_HARD_NPROC})"
+if (( "${VCAP_SOFT_NPROC}" > "${VCAP_HARD_NPROC}" )); then
+  print_err "VCAP_SOFT_NPROC (${VCAP_SOFT_NPROC}) cannot be larger than VCAP_HARD_NPROC (${VCAP_HARD_NPROC})"
   exit 1
 fi
 
-echo "Setting hard nproc limit for vcap: ${DIEGO_VCAP_HARD_NPROC}"
-sed -i "s|\(vcap[ ]*hard[ ]*nproc[ ]*\)[0-9]*|\1${DIEGO_VCAP_HARD_NPROC}|" "${LIMITS_FILEPATH}"
+echo "Setting hard nproc limit for vcap: ${VCAP_HARD_NPROC}"
+sed -i "s|\(vcap[ ]*hard[ ]*nproc[ ]*\)[0-9]*|\1${VCAP_HARD_NPROC}|" "${LIMITS_FILEPATH}"
 
-echo "Setting soft nproc limit for vcap: ${DIEGO_VCAP_SOFT_NPROC}"
-sed -i "s|\(vcap[ ]*soft[ ]*nproc[ ]*\)[0-9]*|\1${DIEGO_VCAP_SOFT_NPROC}|" "${LIMITS_FILEPATH}"
+echo "Setting soft nproc limit for vcap: ${VCAP_SOFT_NPROC}"
+sed -i "s|\(vcap[ ]*soft[ ]*nproc[ ]*\)[0-9]*|\1${VCAP_SOFT_NPROC}|" "${LIMITS_FILEPATH}"
