bd5e3754ec942e433fe538bb4b46d4f2a7cc1025
Thulio Ferraz Assis <tassis@suse.com>
2019-04-08 18:31:32 -0700

    Brain tests - common env vars on testutils

--- src/scf-release/src/acceptance-tests-brain/test-scripts/testutils.rb
+++ src/scf-release/src/acceptance-tests-brain/test-scripts/testutils.rb
@@ -10,6 +10,8 @@ require 'tmpdir'
 # Global options, similar to shopts.
 $opts = { errexit: true, xtrace: true }
 
+NAMESPACE = ENV['KUBERNETES_NAMESPACE']
+CLUSTER_DOMAIN = ENV['KUBERNETES_CLUSTER_DOMAIN']
 STORAGE_CLASS = ENV['KUBERNETES_STORAGE_CLASS_PERSISTENT']
 
 # Set global options.  If a block is given, the options are only active in that
