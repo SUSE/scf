84539b6b4a2e494cd8a8a2470b0d3680b920a22d
Thulio Ferraz Assis <tassis@suse.com>
2019-04-10 14:36:07 -0700

    Bumped fissile

--- bin/common/versions.sh
+++ bin/common/versions.sh
@@ -9,9 +9,8 @@ set -o errexit -o nounset
 
 export BOSH_CLI_VERSION="fcaa9c6caff58ab8da8c56481320681cdea492ee"
 export CFCLI_VERSION="6.42.0"
-export FISSILE_FLAVOR="develop"
-export FISSILE_VERSION="7.0.0+277.gd7b9ebd5"
-
+export FISSILE_FLAVOR="checks"
+export FISSILE_VERSION="7.0.0+283.g4632c215"
 export HELM_VERSION="2.11.0"
 export KK_VERSION="576a42386770423ced46ab4ae9955bee59b0d4dd"
 export KUBECTL_VERSION="1.9.6"
