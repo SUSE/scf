4a59973ca3f3f67d008e4eacbdc17d14bf35b7a8
Mark Yen <mark.yen@suse.com>
2019-04-10 17:09:30 -0700

    role-manifest: allow configgin-roles to patch statefulsets
    This is to implement restarting dependent pods on exported BOSH property
    change.
    
    See also:
    https://github.com/cloudfoundry-incubator/configgin/pull/97
    https://github.com/cloudfoundry-incubator/fissile/pull/470
    https://github.com/SUSE/uaa-fissile-release/pull/153

--- bin/common/versions.sh
+++ bin/common/versions.sh
@@ -27,7 +27,7 @@ export ISTIO_VERSION="1.0.5"
 export STAMPY_MAJOR=$(echo "$STAMPY_VERSION" | sed -e 's/\.g.*//' -e 's/\.[^.]*$//')
 
 # Used in: .envrc
-export FISSILE_STEMCELL_VERSION=${FISSILE_STEMCELL_VERSION:-42.3-30.g9c91e77-30.80}
+export FISSILE_STEMCELL_VERSION=${FISSILE_STEMCELL_VERSION:-42.3-32.g37e6301-30.80}
 
 # Used in: bin/generate-dev-certs.sh
 
