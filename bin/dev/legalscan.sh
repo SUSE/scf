b8f48ff6d1a7e04f1fcacfc54a264384e0361b77
Thulio Ferraz Assis <tassis@suse.com>
2019-05-22 14:15:51 -0700

    Removed bin directory

--- bin/dev/legalscan.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-PROJECTS="Cloud:Platform:sources:scf Cloud:Platform:sources:buildpacks Cloud:Platform:buildpacks:dependencies Cloud:Platform:sources:sidecars"
-for SOURCE_OBS_PROJECT in ${PROJECTS}; do
-  for PACKAGE in $(osc ls ${SOURCE_OBS_PROJECT}); do
-    TARGET_OBS_PROJECT="${SOURCE_OBS_PROJECT}:reviewed"
-    osc submitrequest --yes --message=legal-review ${SOURCE_OBS_PROJECT} ${PACKAGE} ${TARGET_OBS_PROJECT}
-  done
-done
