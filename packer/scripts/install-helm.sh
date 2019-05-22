2b8e8bb36de6ed1c3a8345908439609202b72cb4
Thulio Ferraz Assis <tassis@suse.com>
2019-05-22 14:25:39 -0700

    Removed packer directory

--- packer/scripts/install-helm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -o errexit
-set -o verbose
-
-wget -O - https://kubernetes-helm.storage.googleapis.com/helm-v2.11.0-linux-amd64.tar.gz \
-    | tar xz -C /usr/local/bin --no-same-owner --strip-components=1 linux-amd64/helm
-
-/usr/local/bin/helm init
--- packer/scripts/install-k.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -o errexit
-set -o verbose
-
-# Get predefined version of k
-wget https://github.com/SUSE/kctl/releases/download/v0.0.12/kctl-linux-amd64 -O /usr/local/bin/k
-chmod +x /usr/local/bin/k
