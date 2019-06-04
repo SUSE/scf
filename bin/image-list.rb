3a0c7779e3fca9becc9c3d131ffd14017a337ec0
Jan Dubois <jan@jandubois.com>
2019-06-04 15:15:38 -0700

    CAP-335 Store list of all images inside the helm chart
    This makes is easy to copy them to a local registry to speed up upgrades,
    or to move them to an air-gapped installation.

---
+++ bin/image-list.rb
@@ -0,0 +1,22 @@
+require 'yaml'
+
+helm = ARGV[0]
+values = YAML.load_file(File.join(helm, 'values.yaml'))
+
+images = {}
+Dir.glob(File.join(helm, "templates", "*.yaml")).each do |file|
+  File.open(file).each do |line|
+    /^\s+image:.*kube.organization ?}}\/(.*?)"/.match(line) do |match|
+      images[match[1]] = true
+    end
+    # config variables with the imagename option may point to a bundled image
+    /^\s+value:.*kube.organization "\/" .Values.env.(\S+)/.match(line) do |match|
+      image = values['env'][match[1]]
+      images[image] = true unless image.empty? || image.include?("/")
+    end
+  end
+end
+
+File.open(File.join(helm, "imagelist.txt"), "w") do |file|
+  file.puts(images.keys.sort)
+end
