df111898ef73ccedbc98848500af20ad9dc119ce
Ulrich Kramer <u.kramer@sap.com>
2019-10-01 17:08:10 +0200

    Update eirini to 0.0.17. Works until droplet upload.
    Co-authored-by: Jakob Schmid <jakob.schmid@sap.com>

--- bosh/releases/pre_render_scripts/eirini/opi/jobs/patch_opi_yml.sh
+++ bosh/releases/pre_render_scripts/eirini/opi/jobs/patch_opi_yml.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset
+set -x
 
 # Use kube-native service IPs for the CC Uploader and the
 # Eirini registry
 target="/var/vcap/all-releases/jobs-src/eirini/opi/templates/opi.yml.erb"
+cat $target
+PATCH='@@ -7,9 +7,9 @@
 
-patch --verbose "${target}" <<'EOT'
-10c10
-<   cc_uploader_ip: <%= p("opi.cc_uploader_ip") %>
----
->   cc_uploader_ip: <%= ENV["{{ .Values.deployment_name | upper }}_CC_UPLOADER_SERVICE_HOST"] %>
-12c12
-<   registry_address: <%= p("opi.registry_address") %>
----
->   registry_address: <%= ENV["{{ .Values.deployment_name | upper }}_EIRINI_REGISTRY_SERVICE_HOST"] %>
-EOT
+   cc_certs_secret_name: <%= p("opi.certs_secret_name") %>
+   cc_internal_api: <%= p("opi.cc_internal_api") %>
+-  cc_uploader_ip: <%= p("opi.cc_uploader_ip") %>
++  cc_uploader_ip: <%= ENV["{{ .Values.deployment_name | upper }}_CC_UPLOADER_SERVICE_HOST"] %>
+
+   registry_address: <%= p("opi.registry_address") %>
+   registry_secret_name: bits-service-registry-secret
+   eirini_address: <%= p("opi.eirini_address", "https://" + spec.ip + "opi.tls_port") %>
+   downloader_image: <%= p("opi.downloader_image") %>'
+
+# Only patch once
+if ! patch --reverse --dry-run -f "${target}" <<<"$PATCH" 2>&1  >/dev/null ; then
+  patch --verbose "${target}" <<<"$PATCH"
+else
+  echo "Patch already applied. skipping"
+fi
