639c4f83563d675ba169b9cda4414dce68e6da90
Andreas Kupries <andreas.kupries@suse.com>
2019-10-04 14:13:31 -0700

    More tracing in the swap account checks. (#2877)
    * [jsc#cap-787] More tracing in the swap account checks.
    
    * More commands to introspect the data going into the checks.

--- container-host-files/etc/scf/config/scripts/check_swapaccounting.sh
+++ container-host-files/etc/scf/config/scripts/check_swapaccounting.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
+set -x
+
 # This script checks if swap is available, and if yes, if swapaccounting is
 # enabled on the kernel.  It is an error to have swap without swapaccounting (as
 # that would lead to mis-tracking of memory usage).  If there is no swap,
 # however, not having swap accounting is safe; in that case we turn it off for
 # garden as well.
+
+# First a set of commands to show us what the following checks are seeing.
+
+grep Swap /proc/meminfo ;# Do we have swap, how much ?
+if [ -e /etc/default/grub ] ; then
+    grep CMDLINE /etc/default/grub ;# what does the boot loader say about swap/cgroup ?
+fi
+if [ ! -e /sys/fs/cgroup/memory/memory.memsw.limit_in_bytes ] ; then
+    echo No swap accounting
+fi
+
+# Now the actual checks ...
 
 # If there is no swap, we can drop the swap accounting requirement.
 no_swap="$(awk '/^SwapTotal:/ { print ($2 == 0) ? "true" : "false" }' /proc/meminfo)"
@@ -13,7 +27,7 @@ if [ ! -e /sys/fs/cgroup/memory/memory.memsw.limit_in_bytes ] && ! "${no_swap}" 
 	red='\033[0;31m'
 	no_color='\033[0m'
 	(>&2 printf "${red}")
-	(>&2 printf "cgroup swap accounting is currently not enabled.")
+	(>&2 printf "cgroup swap accounting is currently not enabled, while we have swap.")
 	(>&2 printf " You should enable it on all your k8s nodes by setting the boot option \"swapaccount=1\".")
 	(>&2 printf "${no_color}\\n")
 	sleep 60
