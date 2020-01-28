59fac0a848a504d1a0a3b3e8440df12c0fc43f57
Jan Dubois <jan.dubois@suse.com>
2020-01-27 17:25:52 -0800

    Adjust uaa_ca_cert_reverse_log_proxy_gateway to older release

--- container-host-files/etc/scf/config/scripts/patches/uaa_ca_cert_reverse_log_proxy_gateway.sh
+++ container-host-files/etc/scf/config/scripts/patches/uaa_ca_cert_reverse_log_proxy_gateway.sh
@@ -11,12 +11,12 @@ fi
 patch -d "$PATCH_DIR" --force -p0 <<'PATCH'
 --- bpm.yml.erb
 +++ bpm.yml.erb
-@@ -36,6 +36,10 @@ processes:
+@@ -34,6 +34,10 @@ processes:
        LOG_ADMIN_CLIENT_ID: "<%= p('uaa.client_id') %>"
        LOG_ADMIN_CLIENT_SECRET: "<%= p('uaa.client_secret') %>"
        LOG_ADMIN_ADDR: "<%= p('uaa.internal_addr') %>"
 +      <% if p("uaa.ca_cert") != "" %>
-       LOG_ADMIN_CA_PATH: "<%= certs_dir %>/uaa_ca.crt"
+       LOG_ADMIN_CA_PATH: "/var/vcap/jobs/reverse_log_proxy_gateway/config/certs/uaa_ca.crt"
 +      <% else %>
 +      LOG_ADMIN_CA_PATH: "/var/lib/ca-certificates/ca-bundle.pem"
 +      <% end %>
